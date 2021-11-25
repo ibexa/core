3ad7b93123692484f873e1ada16e52ae7728088c
Dawid Parafiński <dawid.parafinski@ez.no>
2021-11-25 15:34:57 +0100

    IBX-1543: Replaced ez_field function

--- src/lib/MVC/Symfony/Templating/Twig/Extension/ContentExtension.php
+++ src/lib/MVC/Symfony/Templating/Twig/Extension/ContentExtension.php
@@ -82,6 +82,14 @@ class ContentExtension extends AbstractExtension
             ),
             new TwigFunction(
                 'ez_field',
+                [$this, 'getTranslatedField'],
+                [
+                    'deprecated' => '4.0',
+                    'alternative' => 'ibexa_field',
+                ]
+            ),
+            new TwigFunction(
+                'ibexa_field',
                 [$this, 'getTranslatedField']
             ),
             new TwigFunction(
