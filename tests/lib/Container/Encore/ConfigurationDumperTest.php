088581d605d87b089bd615cb7d8bfb51a127929b
Andrew Longosz <alongosz@users.noreply.github.com>
2022-06-21 13:24:13 +0200

    fixup! fixup! [Tests] Implemented unit test for ConfigurationDumper

--- tests/lib/Container/Encore/ConfigurationDumperTest.php
+++ tests/lib/Container/Encore/ConfigurationDumperTest.php
@@ -24,6 +24,7 @@ final class ConfigurationDumperTest extends TestCase
     private Filesystem $filesystem;
 
     private string $projectDir;
+
     private string $fooBarBundlePath;
 
     protected function setUp(): void
