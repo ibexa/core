1d469db7d6182a508b29eabf3e7a0a08ba5c01e3
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2022-09-22 15:24:55 +0200

    IBX-3877: Added logger to cache identifier generation

--- src/lib/Persistence/Cache/Identifier/CacheIdentifierGenerator.php
+++ src/lib/Persistence/Cache/Identifier/CacheIdentifierGenerator.php
@@ -9,13 +9,18 @@ declare(strict_types=1);
 namespace Ibexa\Core\Persistence\Cache\Identifier;
 
 use Ibexa\Core\Base\Exceptions\InvalidArgumentException;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
 
 /**
  * @internal
  */
-final class CacheIdentifierGenerator implements CacheIdentifierGeneratorInterface
+final class CacheIdentifierGenerator implements CacheIdentifierGeneratorInterface, LoggerAwareInterface
 {
     private const PLACEHOLDER = '-%s';
+
+    use LoggerAwareTrait;
 
     /** @var string */
     private $prefix;
@@ -31,6 +36,7 @@ final class CacheIdentifierGenerator implements CacheIdentifierGeneratorInterfac
         $this->prefix = $prefix;
         $this->tagPatterns = $tagPatterns;
         $this->keyPatterns = $keyPatterns;
+        $this->logger = new NullLogger();
     }
 
     /**
@@ -76,6 +82,12 @@ final class CacheIdentifierGenerator implements CacheIdentifierGeneratorInterfac
         if ($withPrefix) {
             $cacheIdentifier = $this->prefix . $cacheIdentifier;
         }
+
+        $this->logger->debug(sprintf('Generated cache identifier: %s', $cacheIdentifier), [
+            'values' => $values,
+            'pattern' => $pattern,
+            'prefix' => $withPrefix ? $this->prefix : null,
+        ]);
 
         return $cacheIdentifier;
     }
