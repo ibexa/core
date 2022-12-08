e63ee2336956f5878511a4211d38884ac5cb2b35
Andrew Longosz <alongosz@users.noreply.github.com>
2022-12-08 15:18:56 +0100

    Implemented SiteAccess-aware Flysystem v2 integration

---
+++ src/lib/IO/Flysystem/PathPrefixer/BaseSiteAccessAwarePathPrefixer.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Core\IO\Flysystem\PathPrefixer;
+
+/**
+ * @internal
+ */
+abstract class BaseSiteAccessAwarePathPrefixer implements PathPrefixerInterface
+{
+    protected string $separator;
+
+    public function __construct(string $separator = \DIRECTORY_SEPARATOR)
+    {
+        $this->separator = $separator;
+    }
+
+    abstract protected function getSiteAccessAwarePathPrefix(): string;
+
+    public function prefixPath(string $path): string
+    {
+        $siteAccessAwarePathPrefix = $this->getSiteAccessAwarePathPrefix();
+        $prefix = rtrim($siteAccessAwarePathPrefix, '\\/');
+        if ($prefix !== '' || $siteAccessAwarePathPrefix === $this->separator) {
+            $prefix .= $this->separator;
+        }
+
+        return $prefix . ltrim($path, '\\/');
+    }
+
+    public function stripPrefix(string $path): string
+    {
+        return substr($path, strlen($this->getSiteAccessAwarePathPrefix()));
+    }
+
+    public function stripDirectoryPrefix(string $path): string
+    {
+        return rtrim($this->stripPrefix($path), '\\/');
+    }
+
+    public function prefixDirectoryPath(string $path): string
+    {
+        $prefixedPath = $this->prefixPath(rtrim($path, '\\/'));
+
+        if ($prefixedPath === '' || (substr($prefixedPath, -1) === $this->separator)) {
+            return $prefixedPath;
+        }
+
+        return $prefixedPath . $this->separator;
+    }
+}
---
+++ src/lib/IO/Flysystem/PathPrefixer/PathPrefixerInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace Ibexa\Core\IO\Flysystem\PathPrefixer;
+
+/**
+ * @internal
+ *
+ * @experimental
+ */
+interface PathPrefixerInterface
+{
+    public function prefixPath(string $path): string;
+
+    public function stripPrefix(string $path): string;
+
+    public function stripDirectoryPrefix(string $path): string;
+
+    public function prefixDirectoryPath(string $path): string;
+}
