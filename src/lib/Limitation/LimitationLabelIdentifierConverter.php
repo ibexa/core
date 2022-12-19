b18ac8ec9e24082df522034828cd6051473a5b7e
Krzysztof Słomka <krzysztof.slomka@ibexa.co>
2022-12-19 10:23:03 +0100

    IBX-4452: Moved logic to convert Limitation Identifier into Label

---
+++ src/lib/Limitation/LimitationLabelIdentifierConverter.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Core\Limitation;
+
+final class LimitationLabelIdentifierConverter
+{
+    public const MESSAGE_ID_PREFIX = 'policy.limitation.identifier.';
+
+    public static function convert(string $identifier): string
+    {
+        return self::MESSAGE_ID_PREFIX . strtolower($identifier);
+    }
+}
