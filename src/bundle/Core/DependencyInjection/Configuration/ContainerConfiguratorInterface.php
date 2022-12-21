b7a121ab7643eef5372c05e15ae8251698386e0a
Andrew Longosz <alongosz@users.noreply.github.com>
2022-12-21 14:43:26 +0100

    Refactored Container configuration for project types

---
+++ src/bundle/Core/DependencyInjection/Configuration/ContainerConfiguratorInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\Core\DependencyInjection\Configuration;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Configurator to be instantiated during Symfony Extension building (load or prepend phase).
+ *
+ * @internal for internal use by Ibexa extensions
+ */
+interface ContainerConfiguratorInterface
+{
+    public function configure(ContainerBuilder $container): void;
+}
