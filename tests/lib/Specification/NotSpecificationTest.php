bf6f063ba191cae56f7ff31ce77b53c53d20dfa8
Nattfarinn <nattfarinn@gmail.com>
2023-01-26 12:27:40 +0100

    fix: Unit test coverage

---
+++ tests/lib/Specification/NotSpecificationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace Ibexa\Tests\Core\Specification;
+
+use Ibexa\Contracts\Core\Specification\NotSpecification;
+
+class NotSpecificationTest extends BaseSpecificationTest
+{
+    public function testNotSpecification(): void
+    {
+        $andSpecification = new NotSpecification(
+            $this->getIsStringSpecification()
+        );
+
+        self::assertFalse($andSpecification->isSatisfiedBy('test'));
+        self::assertTrue($andSpecification->isSatisfiedBy(1234));
+    }
+}
