0975d24062fa70f4bf33f68605def0445fa60834
Tomasz Kryszan <tomasz.kryszan@ez.no>
2023-04-03 15:59:22 +0200

    Added RandomBytesGeneratorTest

---
+++ tests/lib/Token/RandomBytesGeneratorTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Core\Token;
+
+use Ibexa\Contracts\Core\Token\TokenGeneratorInterface;
+use Ibexa\Core\Token\RandomBytesGenerator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Ibexa\Core\Token\RandomBytesGenerator
+ */
+final class RandomBytesGeneratorTest extends TestCase
+{
+    private TokenGeneratorInterface $tokenGenerator;
+
+    protected function setUp(): void
+    {
+        $this->tokenGenerator = new RandomBytesGenerator();
+    }
+
+    /**
+     * @dataProvider provideDataForTestGenerateToken
+     *
+     * @throws \Exception
+     */
+    public function testGenerateToken(
+        int $expectedTokenLengthInBytes,
+        int $length
+    ): void {
+        $generatedToken = $this->tokenGenerator->generateToken($length);
+
+        // Check if Generator returns different tokens
+        self::assertNotSame(
+            $generatedToken,
+            $this->tokenGenerator->generateToken($length)
+        );
+
+        self::assertSame(
+            $expectedTokenLengthInBytes,
+            strlen($generatedToken)
+        );
+    }
+
+    /**
+     * @return iterable<array{
+     *     int,
+     *     int
+     * }>
+     */
+    public function provideDataForTestGenerateToken(): iterable
+    {
+        yield [
+            15,
+            20,
+        ];
+
+        yield [
+            48,
+            64,
+        ];
+
+        yield [
+            75,
+            100,
+        ];
+
+        yield [
+            192,
+            256,
+        ];
+    }
+}
