097cc11e5900aff093a82eee73d8d6ebacde6fe5
Andrew Longosz <alongosz@users.noreply.github.com>
2023-06-30 16:40:39 +0200

    Restored original Helper\NameSchemaService::resolveUrlAliasSchema for BC

--- src/lib/Repository/Helper/NameSchemaService.php
+++ src/lib/Repository/Helper/NameSchemaService.php
@@ -6,6 +6,8 @@
  */
 namespace Ibexa\Core\Repository\Helper;
 
+use Ibexa\Contracts\Core\Repository\Values\Content\Content;
+use Ibexa\Contracts\Core\Repository\Values\ContentType\ContentType;
 use Ibexa\Core\Repository\NameSchema\NameSchemaService as NativeNameSchemaService;
 
 /**
@@ -14,6 +16,17 @@ use Ibexa\Core\Repository\NameSchema\NameSchemaService as NativeNameSchemaServic
  */
 class NameSchemaService extends NativeNameSchemaService
 {
+    public function resolveUrlAliasSchema(Content $content, ContentType $contentType = null): array
+    {
+        $contentType = $contentType ?? $content->getContentType();
+
+        return $this->resolve(
+            empty($contentType->urlAliasSchema) ? $contentType->nameSchema : $contentType->urlAliasSchema,
+            $contentType,
+            $content->fields,
+            $content->versionInfo->languageCodes
+        );
+    }
 }
 
 class_alias(NameSchemaService::class, 'eZ\Publish\Core\Repository\Helper\NameSchemaService');
