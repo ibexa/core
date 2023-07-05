2992a36e3c89c544fabce5c77af7d6591c3d3e2a
Krzysztof Słomka <kisztof@users.noreply.github.com>
2023-07-05 14:10:28 +0200

    Fixed field name generator

--- src/lib/Repository/EventSubscriber/NameSchemaSubscriber.php
+++ src/lib/Repository/EventSubscriber/NameSchemaSubscriber.php
@@ -9,10 +9,18 @@ declare(strict_types=1);
 namespace Ibexa\Core\Repository\EventSubscriber;
 
 use Ibexa\Contracts\Core\Event\ResolveUrlAliasSchemaEvent;
+use Ibexa\Core\FieldType\FieldTypeRegistry;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 final class NameSchemaSubscriber implements EventSubscriberInterface
 {
+    private FieldTypeRegistry $fieldTypeRegistry;
+
+    public function __construct(FieldTypeRegistry $fieldTypeRegistry)
+    {
+        $this->fieldTypeRegistry = $fieldTypeRegistry;
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -22,6 +30,11 @@ final class NameSchemaSubscriber implements EventSubscriberInterface
         ];
     }
 
+    /**
+     * Resolves the URL alias schema by setting token values for specified field identifiers and languages.
+     *
+     * @param \Ibexa\Contracts\Core\Event\ResolveUrlAliasSchemaEvent $event The event object containing the schema identifiers, content, languages, and token values.
+     */
     public function onResolveUrlAliasSchema(ResolveUrlAliasSchemaEvent $event): void
     {
         if (!array_key_exists('field', $event->getSchemaIdentifiers())) {
@@ -32,14 +45,22 @@ final class NameSchemaSubscriber implements EventSubscriberInterface
         $identifiers = $event->getSchemaIdentifiers()['field'];
         $languages = $event->getContent()->getVersionInfo()->getLanguages();
         $tokenValues = $event->getTokenValues();
+
+        $contentType = $content->getContentType();
         foreach ($languages as $language) {
             $languageCode = $language->getLanguageCode();
             foreach ($identifiers as $identifier) {
-                $fieldValue = $content->getFieldValue(
-                    $identifier,
+                $fieldDefinition = $contentType->getFieldDefinition($identifier);
+                $persistenceFieldType = $this->fieldTypeRegistry->getFieldType($fieldDefinition->fieldTypeIdentifier);
+
+                $fieldValue = $content->getFieldValue($identifier, $languageCode);
+                $fieldValue = $persistenceFieldType->getName(
+                    $fieldValue,
+                    $fieldDefinition,
                     $languageCode
                 );
-                $tokenValues[$languageCode][$identifier] = null !== $fieldValue ? (string)$fieldValue : $identifier;
+
+                $tokenValues[$languageCode][$identifier] = $fieldValue;
             }
         }
 
