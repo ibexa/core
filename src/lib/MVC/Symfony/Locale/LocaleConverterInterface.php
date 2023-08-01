70e50cb9d91d326e6fb4b22d9bb30523617e4a53
konradoboza <konrad.oboza@ibexa.co>
2023-08-01 08:24:08 +0200

    Fixed deprecating `convertToEz` method

--- src/lib/MVC/Symfony/Locale/LocaleConverterInterface.php
+++ src/lib/MVC/Symfony/Locale/LocaleConverterInterface.php
@@ -19,9 +19,6 @@ interface LocaleConverterInterface
      * Converts a locale in Ibexa internal format to POSIX format.
      * Returns null if conversion cannot be made.
      *
-     * @deprecated use convertToRepository instead
-     * @see convertToRepository
-     *
      * @param string $ezpLocale
      *
      * @return string|null
@@ -31,6 +28,9 @@ interface LocaleConverterInterface
     /**
      * Converts a locale in POSIX format to Ibexa internal format.
      * Returns null if conversion cannot be made.
+     *
+     * @deprecated use convertToRepository instead
+     * @see convertToRepository
      *
      * @param string $posixLocale
      *
