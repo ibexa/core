be30e09e6be1eead3ed09ec2916cca34358cba69
Adam Wójs <adam@wojs.pl>
2023-10-11 20:15:32 +0200

    IBX-6738: Fixed content type group deletion when it contains orphaned content type drafts (tests)

--- tests/integration/Core/Repository/ContentTypeServiceTest.php
+++ tests/integration/Core/Repository/ContentTypeServiceTest.php
@@ -600,6 +600,40 @@ class ContentTypeServiceTest extends BaseContentTypeServiceTest
         $this->fail('Content type group not deleted.');
     }
 
+    public function testDeleteContentTypeGroupWithOrphanedContentTypeDrafts(): void
+    {
+        $this->expectException(NotFoundException::class);
+
+        $repository = $this->getRepository();
+
+        /* BEGIN: Use Case */
+        $contentTypeService = $repository->getContentTypeService();
+
+        $groupCreate = $contentTypeService->newContentTypeGroupCreateStruct(
+            'new-group'
+        );
+        $contentTypeService->createContentTypeGroup($groupCreate);
+
+        $group = $contentTypeService->loadContentTypeGroupByIdentifier('new-group');
+        for ($i = 0; $i < 3; ++$i) {
+            $contentTypeCreateStruct = $contentTypeService->newContentTypeCreateStruct('content_type_draft_' . $i);
+            $contentTypeCreateStruct->mainLanguageCode = 'eng-GB';
+            $contentTypeCreateStruct->names = [
+                'eng-GB' => 'content_type_draft_' . $i,
+            ];
+
+            $contentTypeService->createContentType($contentTypeCreateStruct, [$group]);
+        }
+
+        $contentTypeService->deleteContentTypeGroup($group);
+        /* END: Use Case */
+
+        // loadContentTypeGroup should throw NotFoundException
+        $contentTypeService->loadContentTypeGroup($group->id);
+
+        $this->fail('Content type group not deleted.');
+    }
+
     /**
      * Test for the newContentTypeCreateStruct() method.
      *
