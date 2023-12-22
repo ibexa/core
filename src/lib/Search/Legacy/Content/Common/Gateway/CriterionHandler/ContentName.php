e34db18378511d7f154d27a73d87fb873d5c8800
Tomasz Kryszan <tomasz.kryszan@ez.no>
2023-12-22 13:00:33 +0100

    Fixes after review

--- src/lib/Search/Legacy/Content/Common/Gateway/CriterionHandler/ContentName.php
+++ src/lib/Search/Legacy/Content/Common/Gateway/CriterionHandler/ContentName.php
@@ -8,28 +8,19 @@ declare(strict_types=1);
 
 namespace Ibexa\Core\Search\Legacy\Content\Common\Gateway\CriterionHandler;
 
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
-use Ibexa\Core\Persistence\TransformationProcessor;
+use Ibexa\Core\Persistence\Legacy\Content\Gateway;
 use Ibexa\Core\Search\Legacy\Content\Common\Gateway\CriteriaConverter;
 use Ibexa\Core\Search\Legacy\Content\Common\Gateway\CriterionHandler;
 
+/**
+ * @internal
+ */
 final class ContentName extends CriterionHandler
 {
     private const CONTENTOBJECT_NAME_ALIAS = 'ezc_n';
     private const CONTENTOBJECT_ALIAS = 'c';
-
-    private TransformationProcessor $transformationProcessor;
-
-    public function __construct(
-        Connection $connection,
-        TransformationProcessor $transformationProcessor
-    ) {
-        parent::__construct($connection);
-
-        $this->transformationProcessor = $transformationProcessor;
-    }
 
     public function accept(Criterion $criterion): bool
     {
@@ -42,6 +33,8 @@ final class ContentName extends CriterionHandler
      *     languages: array<string>,
      *     useAlwaysAvailable: bool,
      *  } $languageSettings
+     *
+     * @throws \Doctrine\DBAL\Exception
      */
     public function handle(
         CriteriaConverter $converter,
@@ -51,69 +44,31 @@ final class ContentName extends CriterionHandler
     ): string {
         $subQuery = $this->connection->createQueryBuilder();
         $subQuery
-            ->select('contentobject_id')
-            ->distinct()
-            ->from('ezcontentobject_name', self::CONTENTOBJECT_NAME_ALIAS)
-            ->innerJoin(
-                self::CONTENTOBJECT_NAME_ALIAS,
-                'ezcontentobject',
-                self::CONTENTOBJECT_ALIAS,
-                $this->getInnerJoinCondition()
-            )
+            ->select('1')
+            ->from(Gateway::CONTENT_NAME_TABLE, self::CONTENTOBJECT_NAME_ALIAS)
             ->andWhere(
+                $queryBuilder->expr()->eq(
+                    self::CONTENTOBJECT_NAME_ALIAS . '.contentobject_id',
+                    self::CONTENTOBJECT_ALIAS . '.id'
+                ),
+                $queryBuilder->expr()->eq(
+                    self::CONTENTOBJECT_NAME_ALIAS . '.content_version',
+                    self::CONTENTOBJECT_ALIAS . '.current_version'
+                ),
                 $queryBuilder->expr()->like(
                     $this->toLowerCase(self::CONTENTOBJECT_NAME_ALIAS . '.name'),
-                    $queryBuilder->createNamedParameter(
-                        $this->prepareValue($criterion)
+                    $this->toLowerCase(
+                        $queryBuilder->createNamedParameter(
+                            $this->prepareValue($criterion)
+                        )
                     )
                 )
             );
 
-        if (!empty($languageSettings['languages'])) {
-            $this->addLanguageConditionToSubQuery(
-                $subQuery,
-                $queryBuilder,
-                $languageSettings['languages']
-            );
-        }
-
-        return $queryBuilder->expr()->in(
-            self::CONTENTOBJECT_ALIAS . '.id',
+        return sprintf(
+            'EXISTS (%s)',
             $subQuery->getSQL()
         );
-    }
-
-    private function getInnerJoinCondition(): string
-    {
-        return sprintf(
-            '(%s = %s AND %s = %s)',
-            self::CONTENTOBJECT_NAME_ALIAS . '.contentobject_id',
-            self::CONTENTOBJECT_ALIAS . '.id',
-            self::CONTENTOBJECT_NAME_ALIAS . '.content_version',
-            self::CONTENTOBJECT_ALIAS . '.current_version',
-        );
-    }
-
-    /**
-     * @param array<string> $languages
-     */
-    private function addLanguageConditionToSubQuery(
-        QueryBuilder $subQuery,
-        QueryBuilder $queryBuilder,
-        array $languages
-    ): void {
-        $subQuery
-            ->andWhere(
-                $queryBuilder->expr()->in(
-                    $this->toLowerCase(self::CONTENTOBJECT_NAME_ALIAS . '.content_translation'),
-                    $this->toLowerCase(
-                        $queryBuilder->createNamedParameter(
-                            $languages,
-                            Connection::PARAM_STR_ARRAY
-                        )
-                    ),
-                )
-            );
     }
 
     private function prepareValue(Criterion $criterion): string
@@ -125,20 +80,17 @@ final class ContentName extends CriterionHandler
             '*',
             '%',
             addcslashes(
-                $this->transformationProcessor->transformByGroup(
-                    $value,
-                    'lowercase'
-                ),
+                $value,
                 '%_'
             )
         );
     }
 
+    /**
+     * @throws \Doctrine\DBAL\Exception
+     */
     private function toLowerCase(string $value): string
     {
-        return sprintf(
-            'LOWER(%s)',
-            $value
-        );
+        return $this->connection->getDatabasePlatform()->getLowerExpression($value);
     }
 }
