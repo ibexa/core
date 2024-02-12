cd31c89c6fa96c3ace5326e9ba4bdf51b4d8313f
tischsoic <symfiz@gmail.com>
2024-02-12 08:12:02 +0100

    IBX-7653: IsContainer criterion added

---
+++ src/lib/Persistence/Legacy/Filter/CriterionQueryBuilder/Location/IsContainerQueryBuilder.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Core\Persistence\Legacy\Filter\CriterionQueryBuilder\Location;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
+use Ibexa\Contracts\Core\Persistence\Filter\Doctrine\FilteringQueryBuilder;
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\IsContainer;
+use Ibexa\Contracts\Core\Repository\Values\Filter\FilteringCriterion;
+use Ibexa\Core\Persistence\Legacy\Content\Type\Gateway;
+
+/**
+ * @internal for internal use by Repository Filtering
+ */
+final class IsContainerQueryBuilder extends BaseLocationCriterionQueryBuilder
+{
+    public function accepts(FilteringCriterion $criterion): bool
+    {
+        return $criterion instanceof IsContainer;
+    }
+
+    public function buildQueryConstraint(
+        FilteringQueryBuilder $queryBuilder,
+        FilteringCriterion $criterion
+    ): ?string {
+        /** @var \Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\IsContainer $criterion */
+        parent::buildQueryConstraint($queryBuilder, $criterion);
+
+        $queryBuilder
+            ->joinOnce(
+                'content',
+                Gateway::CONTENT_TYPE_TABLE,
+                'contentclass',
+                'content.contentclass_id = contentclass.id',
+            );
+
+        $value = $criterion->value ? 1 : 0;
+
+        return $queryBuilder->expr()->in(
+            'contentclass.is_container',
+            $queryBuilder->createNamedParameter($value, ParameterType::INTEGER)
+        );
+    }
+}
