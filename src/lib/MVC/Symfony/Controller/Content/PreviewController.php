4e5b8835700be56195295368c88c8ced4d3770da
Paweł Pawlik <pawel.pawlik@ibexa.co>
2024-02-19 14:22:03 +0100

    IBX-7636: Prevented caching content preview response (#331)
    For more details see https://issues.ibexa.co/browse/IBX-7636 and https://github.com/ibexa/core/pull/331
    
    Key changes:
    
    * Added CacheControl header to preview content response in order to prevent caching preview

--- src/lib/MVC/Symfony/Controller/Content/PreviewController.php
+++ src/lib/MVC/Symfony/Controller/Content/PreviewController.php
@@ -130,6 +130,7 @@ EOF;
                 throw $e;
             }
         }
+        $response->headers->addCacheControlDirective('no-cache', true);
         $response->setPrivate();
 
         $this->previewHelper->restoreConfigScope();
