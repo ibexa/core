482122133ed4f16bff5cdf6a04cb696a54e80a07
konradoboza <konrad.oboza@ibexa.co>
2024-05-09 15:10:30 +0200

    IBX-8140: Enabled authenticator manager-based security

--- src/lib/MVC/Symfony/Security/Authentication/DefaultAuthenticationSuccessHandler.php
+++ src/lib/MVC/Symfony/Security/Authentication/DefaultAuthenticationSuccessHandler.php
@@ -4,19 +4,26 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Core\MVC\Symfony\Security\Authentication;
 
+use Ibexa\Contracts\Core\Repository\PermissionResolver;
 use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use Ibexa\Core\MVC\Symfony\Security\UserInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessHandler as BaseSuccessHandler;
 
-class DefaultAuthenticationSuccessHandler extends BaseSuccessHandler
+final class DefaultAuthenticationSuccessHandler extends BaseSuccessHandler
 {
     private EventDispatcherInterface $eventDispatcher;
 
     private ConfigResolverInterface $configResolver;
+
+    private PermissionResolver $permissionResolver;
 
     /**
      * Injects the ConfigResolver to potentially override default_target_path for redirections after authentication success.
@@ -31,7 +38,22 @@ class DefaultAuthenticationSuccessHandler extends BaseSuccessHandler
         $this->eventDispatcher = $eventDispatcher;
     }
 
-    protected function determineTargetUrl(Request $request)
+    public function setPermissionResolver(PermissionResolver $permissionResolver): void
+    {
+        $this->permissionResolver = $permissionResolver;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): ?Response
+    {
+        $user = $token->getUser();
+        if ($user instanceof UserInterface && isset($this->permissionResolver)) {
+            $this->permissionResolver->setCurrentUserReference($user->getAPIUser());
+        }
+
+        return parent::onAuthenticationSuccess($request, $token);
+    }
+
+    protected function determineTargetUrl(Request $request): string
     {
         if (isset($this->configResolver)) {
             $defaultPage = $this->configResolver->getParameter('default_page');
@@ -41,7 +63,12 @@ class DefaultAuthenticationSuccessHandler extends BaseSuccessHandler
         }
 
         if (isset($this->eventDispatcher)) {
-            $event = new DetermineTargetUrlEvent($request, $this->options, $this->getFirewallName());
+            $event = new DetermineTargetUrlEvent(
+                $request,
+                $this->options,
+                $this->getFirewallName() ?? ''
+            );
+
             $this->eventDispatcher->dispatch($event);
 
             $this->options = $event->getOptions();
