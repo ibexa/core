2710431087f4ecdefffa6159fcc5dada91544516
Andrew Longosz <alongosz@users.noreply.github.com>
2024-05-15 12:24:08 +0200

    [PHPStan] Renamed baseline file for PHP >= 8.0

--- phpstan-baseline.neon.php
+++ phpstan-baseline.neon.php
@@ -10,7 +10,7 @@ $includes = [];
 if (PHP_VERSION_ID < 80000) {
     $includes[] = __DIR__ . '/phpstan-baseline-7.4.neon';
 } else {
-    $includes[] = __DIR__ . '/phpstan-baseline-8.0.neon';
+    $includes[] = __DIR__ . '/phpstan-baseline-gte-8.0.neon';
 }
 
 if (PHP_VERSION_ID < 80200) {
