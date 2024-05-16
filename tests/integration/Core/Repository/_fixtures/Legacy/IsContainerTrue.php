4bcf0c514215b4b4d6517dabc07c3125af4d8707
Jakub Brzegowski <symfiz@gmail.com>
2024-05-16 09:21:26 +0200

    IBX-7653: IsContainer criterion added (#333)

---
+++ tests/integration/Core/Repository/_fixtures/Legacy/IsContainerTrue.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+use Ibexa\Contracts\Core\Repository\Values\Content\Search\SearchHit;
+use Ibexa\Contracts\Core\Repository\Values\Content\Search\SearchResult;
+
+return new SearchResult([
+    'facets' => [],
+    'searchHits' => [
+            new SearchHit([
+                'valueObject' => [
+                        'id' => 4,
+                        'title' => 'Users',
+                    ],
+                'score' => null,
+                'index' => null,
+                'highlight' => null,
+                'matchedTranslation' => 'eng-US',
+            ]),
+            new SearchHit([
+                'valueObject' => [
+                        'id' => 11,
+                        'title' => 'Members',
+                    ],
+                'score' => null,
+                'index' => null,
+                'highlight' => null,
+                'matchedTranslation' => 'eng-US',
+            ]),
+            new SearchHit([
+                'valueObject' => [
+                        'id' => 12,
+                        'title' => 'Administrator users',
+                    ],
+                'score' => null,
+                'index' => null,
+                'highlight' => null,
+                'matchedTranslation' => 'eng-US',
+            ]),
+            new SearchHit([
+                'valueObject' => [
+                        'id' => 13,
+                        'title' => 'Editors',
+                    ],
+                'score' => null,
+                'index' => null,
+                'highlight' => null,
+                'matchedTranslation' => 'eng-US',
+            ]),
+            new SearchHit([
+                'valueObject' => [
+                        'id' => 41,
+                        'title' => 'Media',
+                    ],
+                'score' => null,
+                'index' => null,
+                'highlight' => null,
+                'matchedTranslation' => 'eng-US',
+            ]),
+        ],
+    'spellSuggestion' => null,
+    'time' => 1,
+    'timedOut' => null,
+    'maxScore' => null,
+    'totalCount' => 14,
+]);
