34a8f5fa649037e6b17747c8af60de4f1b7e65ea
Andrew Longosz <alongosz@users.noreply.github.com>
2024-06-11 15:04:12 +0200

    Changed Core Bundle ApiLoader Exception to be Repository Exceptions

--- src/bundle/Core/ApiLoader/Exception/InvalidRepositoryException.php
+++ src/bundle/Core/ApiLoader/Exception/InvalidRepositoryException.php
@@ -4,11 +4,12 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Bundle\Core\ApiLoader\Exception;
 
-use InvalidArgumentException;
+use Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException;
 
-class InvalidRepositoryException extends InvalidArgumentException
+final class InvalidRepositoryException extends InvalidArgumentException
 {
 }
