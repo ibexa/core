cba5558a488139f636dd81e895d11071f9153274
konradoboza <konrad.oboza@ibexa.co>
2024-06-28 16:07:05 +0200

    IBX-8323: Reworked RepositoryAuthenticationProvider and moved its logic to a dedicated subscriber

---
+++ tests/lib/MVC/Symfony/Security/Authentication/EventSubscriber/RepositoryUserAuthenticationSubscriberTest.php
@@ -0,0 +1,152 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Core\MVC\Symfony\Security\Authentication\EventSubscriber;
+
+use Exception;
+use Ibexa\Contracts\Core\Repository\Exceptions\PasswordInUnsupportedFormatException;
+use Ibexa\Contracts\Core\Repository\UserService;
+use Ibexa\Core\MVC\Symfony\Security\Authentication\EventSubscriber\RepositoryUserAuthenticationSubscriber;
+use Ibexa\Core\MVC\Symfony\Security\User;
+use Ibexa\Core\Repository\User\Exception\UnsupportedPasswordHashType;
+use Ibexa\Core\Repository\Values\User\User as APIUser;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Event\CheckPassportEvent;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+final class RepositoryUserAuthenticationSubscriberTest extends TestCase
+{
+    private const int UNSUPPORTED_USER_PASSWORD_HASH_TYPE = 5;
+
+    public function testGetSubscribedEvents(): void
+    {
+        self::assertEquals(
+            [
+                CheckPassportEvent::class => ['validateRepositoryUser'],
+            ],
+            $this->getSubscriber()->getSubscribedEvents()
+        );
+    }
+
+    public function testCheckAuthenticationFailedWhenPasswordInUnsupportedFormat(): void
+    {
+        $apiUser = new APIUser();
+        $user = $this->createMock(User::class);
+        $user
+            ->expects(self::once())
+            ->method('getAPIUser')
+            ->willReturn($apiUser);
+        $user
+            ->method('getPassword')
+            ->willReturn('my_password');
+
+        $userService = $this->createMock(UserService::class);
+        $userService
+            ->expects(self::once())
+            ->method('checkUserCredentials')
+            ->with($apiUser, 'my_password')
+            ->willThrowException(
+                new UnsupportedPasswordHashType(self::UNSUPPORTED_USER_PASSWORD_HASH_TYPE)
+            );
+
+        $this->expectException(PasswordInUnsupportedFormatException::class);
+
+        $this->getSubscriber($userService)->validateRepositoryUser(
+            $this->getCheckPassportEvent($user)
+        );
+    }
+
+    public function testAuthenticateInConstantTime(): void
+    {
+        $constantAuthTime = 1.0;
+        $stopwatch = new Stopwatch();
+        $stopwatch->start('authenticate_constant_time_test');
+
+        try {
+            $this->getSubscriber(null, $constantAuthTime)->validateRepositoryUser(
+                $this->getCheckPassportEvent()
+            );
+        } catch (Exception) {
+            // We don't care, we just need test execution to continue
+        }
+
+        $duration = $stopwatch->stop('authenticate_constant_time_test')->getDuration();
+
+        self::assertGreaterThanOrEqual($constantAuthTime * 1000, $duration);
+    }
+
+    public function testAuthenticateWarningOnConstantTimeExceeded(): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger
+            ->expects(self::once())
+            ->method('warning')
+            ->with('Authentication took longer than the configured constant time. Consider increasing the value of ' . RepositoryUserAuthenticationSubscriber::CONSTANT_AUTH_TIME_SETTING);
+
+        // constant auth time is much too short, but not zero, which would disable the check
+        $this->getSubscriber(null, 0.0000001, $logger)->validateRepositoryUser(
+            $this->getCheckPassportEvent()
+        );
+    }
+
+    public function testAuthenticateConstantTimeDisabled(): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects(self::never())->method('warning');
+
+        $this->getSubscriber(null, 0.0, $logger)->validateRepositoryUser(
+            $this->getCheckPassportEvent()
+        );
+    }
+
+    private function getSubscriber(
+        ?UserService $userService = null,
+        float $constantAuthTime = 1.0,
+        ?LoggerInterface $logger = null
+    ): RepositoryUserAuthenticationSubscriber {
+        $request = $this->createMock(Request::class);
+        $requestStack = $this->createMock(RequestStack::class);
+        $requestStack
+            ->method('getCurrentRequest')
+            ->willReturn($request);
+
+        return new RepositoryUserAuthenticationSubscriber(
+            $requestStack,
+            $userService ?? $this->createMock(UserService::class),
+            $constantAuthTime,
+            $logger ?? $this->createMock(LoggerInterface::class)
+        );
+    }
+
+    private function getCheckPassportEvent(?UserInterface $user = null): CheckPassportEvent
+    {
+        $user = $user ?? $this->createMock(User::class);
+
+        $userProvider = $this->createMock(User\APIUserProviderInterface::class);
+        $userProvider
+            ->expects(self::once())
+            ->method('loadUserByUsername')
+            ->willReturn($user);
+
+        return new CheckPassportEvent(
+            $this->createMock(AuthenticatorInterface::class),
+            new Passport(
+                new UserBadge($user->getUsername(), [$userProvider, 'loadUserByUsername']),
+                new PasswordCredentials($user->getPassword() ?? '')
+            )
+        );
+    }
+}
