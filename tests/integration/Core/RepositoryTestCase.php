960a6714735a29de7cd4cf2ad4361aaba1fe0859
Thorsten Reiter <34274688+reithor@users.noreply.github.com>
2024-07-03 15:43:47 +0200

    Update tests/integration/Core/RepositoryTestCase.php
    Co-authored-by: Paweł Niedzielski <pawel.niedzielski@ibexa.co>

--- tests/integration/Core/RepositoryTestCase.php
+++ tests/integration/Core/RepositoryTestCase.php
@@ -51,7 +51,7 @@ abstract class RepositoryTestCase extends IbexaKernelTestCase
      * @throws \Ibexa\Contracts\Core\Repository\Exceptions\ContentFieldValidationException
      * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
      */
-    protected function createUser(string $login, string $firstName, string $lastName, UserGroup $userGroup = null): User
+    final protected function createUser(string $login, string $firstName, string $lastName, UserGroup $userGroup = null): User
     {
         $userService = self::getUserService();
 
