fd2f7a9242ddfb36f6bbccb0dd10f06b7e498d87
Marek Nocoń <mnocon@users.noreply.github.com>
2024-07-03 15:05:45 +0200

    Update src/contracts/Repository/Values/Content/Relation.php
    Co-authored-by: Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>

--- src/contracts/Repository/Values/Content/Relation.php
+++ src/contracts/Repository/Values/Content/Relation.php
@@ -15,8 +15,8 @@ use Ibexa\Contracts\Core\Repository\Values\ValueObject;
  *
  * @property-read mixed $id the internal id of the relation
  * @property-read string $sourceFieldDefinitionIdentifier the field definition identifier of the field where this relation is anchored if the relation is of type EMBED, LINK, or ATTRIBUTE
- * @property-read \Ibexa\Contracts\Core\Repository\Values\Content\ContentInfo $sourceContentInfo - calls {@see Relation::getSourceContentInfo()}
- * @property-read \Ibexa\Contracts\Core\Repository\Values\Content\ContentInfo $destinationContentInfo - calls {@see Relation::getDestinationContentInfo()}
+ * @property-read \Ibexa\Contracts\Core\Repository\Values\Content\ContentInfo $sourceContentInfo Calls {@see Relation::getSourceContentInfo()}
+ * @property-read \Ibexa\Contracts\Core\Repository\Values\Content\ContentInfo $destinationContentInfo Calls {@see Relation::getDestinationContentInfo()}
  * @property-read int $type The relation type bitmask containing one or more of Relation::COMMON, Relation::EMBED, Relation::LINK, Relation::FIELD
  */
 abstract class Relation extends ValueObject
