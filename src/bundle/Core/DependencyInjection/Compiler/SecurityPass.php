fe4c34ac626ff6a522fd6301aad6f972d9149249
Konrad Oboza <konrad.oboza@ibexa.co>
2024-07-09 11:37:31 +0200

    IBX-8323: Reworked RepositoryAuthenticationProvider and moved its logic to a dedicated subscriber (#396)
    For more details see https://issues.ibexa.co/browse/IBX-8323 and https://github.com/ibexa/core/pull/396
    
    Key changes:
    
    * Reworked RepositoryAuthenticationProvider and moved its logic to a dedicated subscriber

--- src/bundle/Core/DependencyInjection/Compiler/SecurityPass.php
+++ src/bundle/Core/DependencyInjection/Compiler/SecurityPass.php
@@ -4,6 +4,7 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Bundle\Core\DependencyInjection\Compiler;
 
@@ -11,7 +12,6 @@ use Ibexa\Contracts\Core\Repository\PermissionResolver;
 use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
 use Ibexa\Core\MVC\Symfony\Security\Authentication\DefaultAuthenticationSuccessHandler;
 use Ibexa\Core\MVC\Symfony\Security\Authentication\GuardRepositoryAuthenticationProvider;
-use Ibexa\Core\MVC\Symfony\Security\Authentication\RememberMeRepositoryAuthenticationProvider;
 use Ibexa\Core\MVC\Symfony\Security\HttpUtils;
 use Ibexa\Core\MVC\Symfony\SiteAccess;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -24,30 +24,15 @@ use Symfony\Component\DependencyInjection\Reference;
  */
 final class SecurityPass implements CompilerPassInterface
 {
-    /**
-     * @deprecated 4.6.7 CONSTANT_AUTH_TIME_SETTING constant is deprecated, will be removed in 5.0.
-     */
-    public const string CONSTANT_AUTH_TIME_SETTING = 'ibexa.security.authentication.constant_auth_time';
-
-    public const float CONSTANT_AUTH_TIME_DEFAULT = 1.0;
-
     public function process(ContainerBuilder $container): void
     {
         if (
-            !$container->hasDefinition('security.authentication.provider.rememberme') ||
             !$container->hasDefinition('security.authentication.provider.guard')
         ) {
             return;
         }
 
         $permissionResolverRef = new Reference(PermissionResolver::class);
-
-        $rememberMeAuthenticationProviderDef = $container->findDefinition('security.authentication.provider.rememberme');
-        $rememberMeAuthenticationProviderDef->setClass(RememberMeRepositoryAuthenticationProvider::class);
-        $rememberMeAuthenticationProviderDef->addMethodCall(
-            'setPermissionResolver',
-            [$permissionResolverRef]
-        );
 
         $guardAuthenticationProviderDef = $container->findDefinition('security.authentication.provider.guard');
         $guardAuthenticationProviderDef->setClass(GuardRepositoryAuthenticationProvider::class);
