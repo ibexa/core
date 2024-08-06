ac20ef32eeda4f24a89a147b39fff9d1c626ba07
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2024-08-06 21:09:47 +0200

    IBX-8418: Fixed integration test

--- tests/integration/Core/Repository/TrashService/TrashTest.php
+++ tests/integration/Core/Repository/TrashService/TrashTest.php
@@ -27,7 +27,6 @@ final class TrashTest extends RepositoryTestCase
 
         $folder = $this->createFolder(['eng-GB' => 'Folder'], 2);
         $folderMainLocationId = $folder->getVersionInfo()->getContentInfo()->getMainLocationId();
-
         Assert::assertIsNumeric($folderMainLocationId);
 
         $childFolder = $this->createFolder(
@@ -35,24 +34,47 @@ final class TrashTest extends RepositoryTestCase
             $folderMainLocationId,
         );
         $childFolderMainLocationId = $childFolder->getVersionInfo()->getContentInfo()->getMainLocationId();
-
         Assert::assertIsNumeric($childFolderMainLocationId);
+
+        $secondDepthChildFolder = $this->createFolder(
+            ['eng-GB' => 'Second depth folder'],
+            $childFolderMainLocationId,
+        );
+        $secondDepthChildFolderLocationId = $secondDepthChildFolder
+            ->getVersionInfo()
+            ->getContentInfo()
+            ->getMainLocationId()
+        ;
+        Assert::assertIsNumeric($secondDepthChildFolderLocationId);
 
         $draft1 = $this->createFolderDraft(['eng-GB' => 'Folder draft 1'], $folderMainLocationId);
         $draft2 = $this->createFolderDraft(['eng-GB' => 'Folder draft 2'], $childFolderMainLocationId);
         $draft3 = $this->createFolderDraft(['eng-GB' => 'Folder draft 3'], $childFolderMainLocationId);
+        $draftSecondDepth = $this->createFolderDraft(
+            ['eng-GB' => 'Folder draft 4'],
+            $secondDepthChildFolderLocationId,
+        );
 
         $locationToTrash = $this->getLocationService()->loadLocation($folderMainLocationId);
 
         $trashService->trash($locationToTrash);
 
-        $this->expectException(NotFoundException::class);
-        $contentService->loadContentInfo($draft1->getId());
+        $draftIds = [
+            $draft1->getId(),
+            $draft2->getId(),
+            $draft3->getId(),
+            $draftSecondDepth->getId(),
+        ];
 
-        $this->expectException(NotFoundException::class);
-        $contentService->loadContentInfo($draft2->getId());
-
-        $this->expectException(NotFoundException::class);
-        $contentService->loadContentInfo($draft3->getId());
+        foreach ($draftIds as $draftId) {
+            try {
+                $contentService->loadContentInfo($draftId);
+                $this->fail(
+                    sprintf('Expected NotFoundException not thrown for draft ID = %d', $draftId),
+                );
+            } catch (NotFoundException $e) {
+                self::assertInstanceOf(NotFoundException::class, $e);
+            }
+        }
     }
 }
