aff09d26e08cb242cc36094693ba702e1735cd99
Tomasz Kryszan <tomasz.kryszan@ibexa.co>
2024-08-16 12:27:11 +0200

    Added IsBookmarkedValue

---
+++ src/contracts/Repository/Values/Content/Query/Criterion/Value/IsBookmarkedValue.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\Value;
+
+use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion\Value;
+
+final class IsBookmarkedValue extends Value
+{
+    private ?int $userId;
+
+    public function __construct(?int $userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+}
