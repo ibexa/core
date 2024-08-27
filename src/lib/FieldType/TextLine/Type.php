13a0cf16271d526d94f167825d323214787fb3eb
Andrew Longosz <alongosz@users.noreply.github.com>
2024-08-27 11:27:47 +0200

    Extracted common base for TextBlock and TextLine field types (#406)
    For more details see https://github.com/ibexa/core/pull/406
    
    Key changes:
    
    * Extracted common base for TextBlock and TextLine field types
    
    * [Tests] Aligned tests with TextBlock and TextLine changes
    
    * [Tests] Reduced complexity of TextBlock and TextLine test classes
    
    * [PHPStan] Aligned baseline with the changes
    
    ---------
    
    Co-authored-by: Paweł Niedzielski <Steveb-p@users.noreply.github.com>

--- src/lib/FieldType/TextLine/Type.php
+++ src/lib/FieldType/TextLine/Type.php
@@ -7,22 +7,20 @@
 
 namespace Ibexa\Core\FieldType\TextLine;
 
+use Ibexa\Contracts\Core\Exception\InvalidArgumentType;
 use Ibexa\Contracts\Core\FieldType\Value as SPIValue;
 use Ibexa\Contracts\Core\Repository\Values\ContentType\FieldDefinition;
-use Ibexa\Core\Base\Exceptions\InvalidArgumentType;
-use Ibexa\Core\FieldType\FieldType;
-use Ibexa\Core\FieldType\ValidationError;
+use Ibexa\Core\FieldType\BaseTextType;
 use Ibexa\Core\FieldType\Validator\StringLengthValidator;
 use Ibexa\Core\FieldType\Value as BaseValue;
 use JMS\TranslationBundle\Model\Message;
-use JMS\TranslationBundle\Translation\TranslationContainerInterface;
 
 /**
  * The TextLine field type.
  *
  * This field type represents a simple string.
  */
-class Type extends FieldType implements TranslationContainerInterface
+class Type extends BaseTextType
 {
     protected $validatorConfigurationSchema = [
         'StringLengthValidator' => [
@@ -40,27 +38,21 @@ class Type extends FieldType implements TranslationContainerInterface
     /**
      * Validates the validatorConfiguration of a FieldDefinitionCreateStruct or FieldDefinitionUpdateStruct.
      *
-     * @param mixed $validatorConfiguration
+     * @param array<string, mixed> $validatorConfiguration
      *
      * @return \Ibexa\Contracts\Core\FieldType\ValidationError[]
      */
-    public function validateValidatorConfiguration($validatorConfiguration)
+    public function validateValidatorConfiguration($validatorConfiguration): array
     {
         $validationErrors = [];
-        $validator = new StringLengthValidator();
+        $validators = ['StringLengthValidator' => new StringLengthValidator()];
 
         foreach ($validatorConfiguration as $validatorIdentifier => $constraints) {
-            if ($validatorIdentifier !== 'StringLengthValidator') {
-                $validationErrors[] = new ValidationError(
-                    "Validator '%validator%' is unknown",
-                    null,
-                    [
-                        '%validator%' => $validatorIdentifier,
-                    ]
-                );
+            if (!isset($validators[$validatorIdentifier])) {
+                $validationErrors[] = $this->buildUnknownValidatorError('%validator%', $validatorIdentifier);
                 continue;
             }
-            $validationErrors += $validator->validateConstraints($constraints);
+            $validationErrors += $validators[$validatorIdentifier]->validateConstraints($constraints);
         }
 
         return $validationErrors;
@@ -69,14 +61,14 @@ class Type extends FieldType implements TranslationContainerInterface
     /**
      * Validates a field based on the validators in the field definition.
      *
-     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException
-     *
      * @param \Ibexa\Contracts\Core\Repository\Values\ContentType\FieldDefinition $fieldDefinition The field definition of the field
      * @param \Ibexa\Core\FieldType\TextLine\Value $fieldValue The field value for which an action is performed
      *
      * @return \Ibexa\Contracts\Core\FieldType\ValidationError[]
+     *
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\PropertyNotFoundException
      */
-    public function validate(FieldDefinition $fieldDefinition, SPIValue $fieldValue)
+    public function validate(FieldDefinition $fieldDefinition, SPIValue $fieldValue): array
     {
         $validationErrors = [];
 
@@ -85,85 +77,27 @@ class Type extends FieldType implements TranslationContainerInterface
         }
 
         $validatorConfiguration = $fieldDefinition->getValidatorConfiguration();
-        $constraints = isset($validatorConfiguration['StringLengthValidator'])
-            ? $validatorConfiguration['StringLengthValidator']
-            : [];
+        $constraints = $validatorConfiguration['StringLengthValidator'] ?? [];
+        $validator = new StringLengthValidator();
+        $validator->initializeWithConstraints($constraints);
 
-        if (isset($constraints['maxStringLength']) &&
-            $constraints['maxStringLength'] !== false &&
-            $constraints['maxStringLength'] !== 0 &&
-            mb_strlen($fieldValue->text) > $constraints['maxStringLength']) {
-            $validationErrors[] = new ValidationError(
-                'The string can not exceed %size% character.',
-                'The string can not exceed %size% characters.',
-                [
-                    '%size%' => $constraints['maxStringLength'],
-                ],
-                'text'
-            );
-        }
-
-        if (isset($constraints['minStringLength']) &&
-            $constraints['minStringLength'] !== false &&
-            $constraints['minStringLength'] !== 0 &&
-            mb_strlen($fieldValue->text) < $constraints['minStringLength']) {
-            $validationErrors[] = new ValidationError(
-                'The string cannot be shorter than %size% character.',
-                'The string cannot be shorter than %size% characters.',
-                [
-                    '%size%' => $constraints['minStringLength'],
-                ],
-                'text'
-            );
-        }
-
-        return $validationErrors;
+        return false === $validator->validate($fieldValue, $fieldDefinition) ? $validator->getMessage() : [];
     }
 
-    /**
-     * Returns the field type identifier for this field type.
-     *
-     * @return string
-     */
     public function getFieldTypeIdentifier()
     {
         return 'ezstring';
     }
 
     /**
-     * @param \Ibexa\Core\FieldType\TextLine\Value|\Ibexa\Contracts\Core\FieldType\Value $value
-     */
-    public function getName(SPIValue $value, FieldDefinition $fieldDefinition, string $languageCode): string
-    {
-        return (string)$value->text;
-    }
-
-    /**
-     * Returns the fallback default value of field type when no such default
-     * value is provided in the field definition in content types.
-     *
      * @return \Ibexa\Core\FieldType\TextLine\Value
      */
-    public function getEmptyValue()
+    public function getEmptyValue(): Value
     {
         return new Value();
     }
 
     /**
-     * Returns if the given $value is considered empty by the field type.
-     *
-     * @param mixed $value
-     *
-     * @return bool
-     */
-    public function isEmptyValue(SPIValue $value)
-    {
-        return $value->text === null || trim($value->text) === '';
-    }
-
-    /**
-     * Inspects given $inputValue and potentially converts it into a dedicated value object.
-     *
      * @param string|\Ibexa\Core\FieldType\TextLine\Value $inputValue
      *
      * @return \Ibexa\Core\FieldType\TextLine\Value The potentially converted and structurally plausible value.
@@ -177,21 +111,10 @@ class Type extends FieldType implements TranslationContainerInterface
         return $inputValue;
     }
 
-    /**
-     * Throws an exception if value structure is not of expected format.
-     *
-     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException If the value does not match the expected structure.
-     *
-     * @param \Ibexa\Core\FieldType\TextLine\Value $value
-     */
-    protected function checkValueStructure(BaseValue $value)
+    protected static function checkValueType(mixed $value): void
     {
-        if (!is_string($value->text)) {
-            throw new InvalidArgumentType(
-                '$value->text',
-                'string',
-                $value->text
-            );
+        if (!$value instanceof Value) {
+            throw new InvalidArgumentType('$value', Value::class, $value);
         }
     }
 
@@ -199,22 +122,16 @@ class Type extends FieldType implements TranslationContainerInterface
      * Returns information for FieldValue->$sortKey relevant to the field type.
      *
      * @param \Ibexa\Core\FieldType\TextLine\Value $value
-     *
-     * @return string
      */
-    protected function getSortInfo(BaseValue $value)
+    protected function getSortInfo(BaseValue $value): string
     {
         return $this->transformationProcessor->transformByGroup((string)$value, 'lowercase');
     }
 
     /**
-     * Converts an $hash to the Value defined by the field type.
-     *
-     * @param mixed $hash
-     *
-     * @return \Ibexa\Core\FieldType\TextLine\Value $value
+     * @param string $hash
      */
-    public function fromHash($hash)
+    public function fromHash($hash): Value
     {
         if ($hash === null) {
             return $this->getEmptyValue();
@@ -224,31 +141,8 @@ class Type extends FieldType implements TranslationContainerInterface
     }
 
     /**
-     * Converts a $Value to a hash.
-     *
-     * @param \Ibexa\Core\FieldType\TextLine\Value $value
-     *
-     * @return mixed
+     * @return list<\JMS\TranslationBundle\Model\Message>
      */
-    public function toHash(SPIValue $value)
-    {
-        if ($this->isEmptyValue($value)) {
-            return null;
-        }
-
-        return $value->text;
-    }
-
-    /**
-     * Returns whether the field type is searchable.
-     *
-     * @return bool
-     */
-    public function isSearchable()
-    {
-        return true;
-    }
-
     public static function getTranslationMessages(): array
     {
         return [
