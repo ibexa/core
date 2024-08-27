202d3215bb4d038a3d0db9645c6abd2a0c89e343
Andrew Longosz <alongosz@users.noreply.github.com>
2024-08-27 12:55:42 +0200

    [Tests] Moved Float, Integer, String Validator tests to proper namespace

--- tests/lib/FieldType/Validator/IntegerValueValidatorTest.php
+++ tests/lib/FieldType/Validator/IntegerValueValidatorTest.php
@@ -6,7 +6,7 @@
  */
 declare(strict_types=1);
 
-namespace Ibexa\Tests\Core\FieldType;
+namespace Ibexa\Tests\Core\FieldType\Validator;
 
 use Ibexa\Contracts\Core\FieldType\ValidationError;
 use Ibexa\Contracts\Core\Repository\Exceptions\PropertyNotFoundException;
