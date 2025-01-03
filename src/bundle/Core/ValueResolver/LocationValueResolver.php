e426c696ddef5664016fd057afd2a877295c6438
Dawid Parafinski <dawid.parafinski@ibexa.co>
2025-01-03 12:47:08 +0100

    Replaced Param Converters with Value Resolvers

---
+++ src/bundle/Core/ValueResolver/LocationValueResolver.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Bundle\Core\ValueResolver;
+
+use Ibexa\Contracts\Core\Repository\LocationService;
+use Ibexa\Contracts\Core\Repository\Values\Content\Language;
+use Ibexa\Contracts\Core\Repository\Values\Content\Location;
+use Ibexa\Core\Helper\ContentPreviewHelper;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+final class LocationValueResolver implements ValueResolverInterface
+{
+    private const string ATTRIBUTE_LOCATION_ID = 'locationId';
+
+    public function __construct(
+        private readonly LocationService $locationService,
+        private readonly ContentPreviewHelper $contentPreviewHelper
+    ) {
+    }
+
+    /**
+     * @return iterable<\Ibexa\Contracts\Core\Repository\Values\Content\Location>
+     *
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException
+     * @throws \Ibexa\Contracts\Core\Repository\Exceptions\UnauthorizedException
+     */
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
+    {
+        if ($argument->getType() !== Location::class) {
+            return [];
+        }
+
+        $locationId = $request->attributes->get(self::ATTRIBUTE_LOCATION_ID);
+
+        if (!is_numeric($locationId)) {
+            return [];
+        }
+
+        $prioritizedLanguages = $this->contentPreviewHelper->isPreviewActive() ? Language::ALL : null;
+
+        yield $this->locationService->loadLocation((int)$locationId, $prioritizedLanguages);
+    }
+}
---
+++ tests/bundle/Core/ValueResolver/ContentValueResolverTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Bundle\Core\ValueResolver;
+
+use Ibexa\Bundle\Core\ValueResolver\ContentValueResolver;
+use Ibexa\Contracts\Core\Repository\ContentService;
+use Ibexa\Contracts\Core\Repository\Values\Content\Content;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+final class ContentValueResolverTest extends TestCase
+{
+    private ContentValueResolver $resolver;
+
+    private MockObject&ContentService $contentServiceMock;
+
+    protected function setUp(): void
+    {
+        $this->contentServiceMock = $this->createMock(ContentService::class);
+        $this->resolver = new ContentValueResolver($this->contentServiceMock);
+    }
+
+    public function testResolveWithValidContentId(): void
+    {
+        $request = new Request([], [], ['contentId' => '123']);
+        $argumentMetadata = $this->createMock(ArgumentMetadata::class);
+        $argumentMetadata->method('getType')->willReturn(Content::class);
+
+        $mockContent = $this->createMock(Content::class);
+
+        $this->contentServiceMock
+            ->expects(self::once())
+            ->method('loadContent')
+            ->with(123)
+            ->willReturn($mockContent);
+
+        $result = iterator_to_array($this->resolver->resolve($request, $argumentMetadata));
+
+        self::assertSame([$mockContent], $result);
+    }
+
+    public function testResolveWithInvalidContentId(): void
+    {
+        $request = new Request([], [], ['contentId' => 'invalid']);
+        $argumentMetadata = $this->createMock(ArgumentMetadata::class);
+        $argumentMetadata->method('getType')->willReturn(Content::class);
+
+        $this->contentServiceMock
+            ->expects(self::never())
+            ->method('loadContent');
+
+        $result = iterator_to_array($this->resolver->resolve($request, $argumentMetadata));
+
+        self::assertSame([], $result);
+    }
+
+    public function testResolveWithNonContentType(): void
+    {
+        $request = new Request([], [], ['contentId' => '123']);
+        $argumentMetadata = $this->createMock(ArgumentMetadata::class);
+        $argumentMetadata->method('getType')->willReturn('OtherClass');
+
+        $this->contentServiceMock
+            ->expects(self::never())
+            ->method('loadContent');
+
+        $result = iterator_to_array($this->resolver->resolve($request, $argumentMetadata));
+
+        self::assertSame([], $result);
+    }
+}
