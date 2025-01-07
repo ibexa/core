1b346c98ca7deb9e1121b2da326c0798dc637fed
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2025-01-07 10:09:14 +0100

    Added strict_types
    Co-authored-by: Konrad Oboza <konrad.oboza@ibexa.co>

--- tests/integration/Core/Repository/SerializationTest.php
+++ tests/integration/Core/Repository/SerializationTest.php
@@ -3,6 +3,8 @@
 /**
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
+declare(strict_types=1);
+
  */
 namespace Ibexa\Tests\Integration\Core\Repository;
 
