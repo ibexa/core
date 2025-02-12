b43570078aed9010c8af6a025d980c0f28e2cfb1
Mateusz Kolankowski <wiewiurdp@tlen.pl>
2025-02-12 08:21:46 +0100

    IBX-9415: Added support for ContentAwareInterface in ibexa_* Twig functions (#467)

--- src/lib/MVC/Symfony/Templating/Twig/Extension/RenderContentExtension.php
+++ src/lib/MVC/Symfony/Templating/Twig/Extension/RenderContentExtension.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 namespace Ibexa\Core\MVC\Symfony\Templating\Twig\Extension;
 
 use Ibexa\Contracts\Core\Repository\Values\Content\Content;
+use Ibexa\Contracts\Core\Repository\Values\Content\ContentAwareInterface;
+use Ibexa\Core\Base\Exceptions\InvalidArgumentType;
 use Ibexa\Core\MVC\Symfony\Event\ResolveRenderOptionsEvent;
 use Ibexa\Core\MVC\Symfony\Templating\RenderContentStrategy;
 use Ibexa\Core\MVC\Symfony\Templating\RenderOptions;
@@ -58,14 +60,39 @@ final class RenderContentExtension extends AbstractExtension
         ];
     }
 
-    public function renderContent(Content $content, array $options = []): string
+    /**
+     * @param \Ibexa\Contracts\Core\Repository\Values\Content\Content|\Ibexa\Contracts\Core\Repository\Values\Content\ContentAwareInterface $data
+     */
+    public function renderContent(object $data, array $options = []): string
     {
         $renderOptions = new RenderOptions($options);
         $event = $this->eventDispatcher->dispatch(
             new ResolveRenderOptionsEvent($renderOptions)
         );
 
-        return $this->renderContentStrategy->render($content, $event->getRenderOptions());
+        return $this->renderContentStrategy->render($this->getContent($data), $event->getRenderOptions());
+    }
+
+    /**
+     * @param \Ibexa\Contracts\Core\Repository\Values\Content\Content|\Ibexa\Contracts\Core\Repository\Values\Content\ContentAwareInterface $data
+     *
+     * @throws \Ibexa\Core\Base\Exceptions\InvalidArgumentType
+     */
+    private function getContent(object $data): Content
+    {
+        if ($data instanceof Content) {
+            return $data;
+        }
+
+        if ($data instanceof ContentAwareInterface) {
+            return $data->getContent();
+        }
+
+        throw new InvalidArgumentType(
+            '$content',
+            sprintf('%s or %s', Content::class, ContentAwareInterface::class),
+            $data,
+        );
     }
 }
 
