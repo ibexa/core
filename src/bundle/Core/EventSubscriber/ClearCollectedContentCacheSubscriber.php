7ebad66df4fd07d0bc2af83f6ebc9cf4c8f0d386
Dawid Parafinski <dawid.parafinski@ibexa.co>
2025-04-03 08:31:17 +0200

    marked Subscriber as final

--- src/bundle/Core/EventSubscriber/ClearCollectedContentCacheSubscriber.php
+++ src/bundle/Core/EventSubscriber/ClearCollectedContentCacheSubscriber.php
@@ -15,7 +15,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-class ClearCollectedContentCacheSubscriber implements EventSubscriberInterface
+final class ClearCollectedContentCacheSubscriber implements EventSubscriberInterface
 {
     private TransactionAwareAdapterInterface $cache;
 
