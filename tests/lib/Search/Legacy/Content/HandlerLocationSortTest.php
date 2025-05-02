4d600b6057d71636955d3e2c398d7c92abb194db
Adam Wójs <adam@wojs.pl>
2025-05-02 19:59:21 +0200

    Fixed Ibexa\Tests\Core\Search\Legacy\Content\HandlerLocationSortTest::testSortLocationVisibilityAscending test

--- tests/lib/Search/Legacy/Content/HandlerLocationSortTest.php
+++ tests/lib/Search/Legacy/Content/HandlerLocationSortTest.php
@@ -421,7 +421,7 @@ class HandlerLocationSortTest extends AbstractTestCase
                 [
                     'filter' => new Criterion\LocationId([45, 228]),
                     'offset' => 0,
-                    'limit' => null,
+                    'limit' => 999,
                     'sortClauses' => [
                         new SortClause\Location\Visibility(LocationQuery::SORT_ASC),
                     ],
