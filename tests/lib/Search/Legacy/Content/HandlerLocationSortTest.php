7304c48c30dbfb78cce1704c0e1a6d04d6cfccd1
Adam Wójs <adam@wojs.pl>
2025-05-02 19:58:56 +0200

    Fixed Ibexa\Tests\Core\Search\Legacy\Content\HandlerLocationSortTest::testSortLocationId test

--- tests/lib/Search/Legacy/Content/HandlerLocationSortTest.php
+++ tests/lib/Search/Legacy/Content/HandlerLocationSortTest.php
@@ -398,7 +398,7 @@ class HandlerLocationSortTest extends AbstractTestCase
                 [
                     'filter' => new Criterion\LocationId([13, 15, 44, 45, 228]),
                     'offset' => 0,
-                    'limit' => null,
+                    'limit' => 999,
                     'sortClauses' => [
                         new SortClause\Location\Id(LocationQuery::SORT_DESC),
                     ],
