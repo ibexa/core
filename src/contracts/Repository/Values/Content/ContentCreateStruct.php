b76de646458a500225a070c9081ba8bd815b1550
Adam Wójs <adam@wojs.pl>
2025-05-31 21:39:52 +0200

    Added missing type hints to Ibexa\Contracts\Core\Repository\Values\Content\ContentCreateStruct

--- src/contracts/Repository/Values/Content/ContentCreateStruct.php
+++ src/contracts/Repository/Values/Content/ContentCreateStruct.php
@@ -8,6 +8,9 @@ declare(strict_types=1);
 
 namespace Ibexa\Contracts\Core\Repository\Values\Content;
 
+use DateTimeInterface;
+use Ibexa\Contracts\Core\Repository\Values\ContentType\ContentType;
+
 /**
  * This class is used for creating a new content object.
  *
@@ -19,41 +22,8 @@ abstract class ContentCreateStruct extends ContentStruct
      * The content type for which the new content is created.
      *
      * Required.
-     *
-     * @var \Ibexa\Contracts\Core\Repository\Values\ContentType\ContentType
      */
-    public $contentType;
-
-    /**
-     * The section the content is assigned to.
-     * If not set the section of the parent is used or a default section.
-     *
-     * @var mixed
-     */
-    public $sectionId;
-
-    /**
-     * The owner of the content. If not given the current authenticated user is set as owner.
-     *
-     * @var mixed
-     */
-    public $ownerId;
-
-    /**
-     * Indicates if the content object is shown in the mainlanguage if its not present in an other requested language.
-     *
-     * @var bool
-     */
-    public $alwaysAvailable;
-
-    /**
-     * Remote identifier used as a custom identifier for the object.
-     *
-     * Needs to be a unique Content->remoteId string value.
-     *
-     * @var string
-     */
-    public $remoteId;
+    public ContentType $contentType;
 
     /**
      * the main language code for the content. This language will also
@@ -61,15 +31,34 @@ abstract class ContentCreateStruct extends ContentStruct
      * It is also used as default language for added fields.
      *
      * Required.
-     *
-     * @var string
      */
-    public $mainLanguageCode;
+    public string $mainLanguageCode;
+
+    /**
+     * The section the content is assigned to.
+     * If not set the section of the parent is used or a default section.
+     */
+    public ?int $sectionId = null;
+
+    /**
+     * The owner of the content. If not given the current authenticated user is set as owner.
+     */
+    public ?int $ownerId = null;
+
+    /**
+     * Indicates if the content object is shown in the mainlanguage if its not present in an other requested language.
+     */
+    public ?bool $alwaysAvailable = null;
+
+    /**
+     * Remote identifier used as a custom identifier for the object.
+     *
+     * Needs to be a unique Content->remoteId string value.
+     */
+    public ?string $remoteId = null;
 
     /**
      * Modification date. If not given the current timestamp is used.
-     *
-     * @var \DateTime
      */
-    public $modificationDate;
+    public ?DateTimeInterface $modificationDate = null;
 }
