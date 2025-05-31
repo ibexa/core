11c7dd69b12448d53fe3f0dd65bd06e2832e08d1
Adam Wójs <adam@wojs.pl>
2025-05-31 21:48:32 +0200

    Added missing type hints to Ibexa\Contracts\Core\Repository\Values\Content\ContentStruct

--- src/lib/Repository/Values/User/UserGroupCreateStruct.php
+++ src/lib/Repository/Values/User/UserGroupCreateStruct.php
@@ -37,7 +37,7 @@ class UserGroupCreateStruct extends APIUserGroupCreateStruct
      *                     field type or an instance of a Value class provided by the field type
      * @param string|null $language If not given on a translatable field the initial language is used
      */
-    public function setField(string $fieldDefIdentifier, $value, ?string $language = null): void
+    public function setField(string $fieldDefIdentifier, mixed $value, ?string $language = null): void
     {
         if (!isset($language)) {
             $language = $this->mainLanguageCode;
