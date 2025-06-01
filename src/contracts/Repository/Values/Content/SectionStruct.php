9c72401fd61320e41b85a0ed522874a2ad0c7638
Adam Wójs <adam@wojs.pl>
2025-06-01 08:20:53 +0200

    Added missing type hints to Ibexa\Contracts\Core\Repository\Values\Content\SectionStruct

--- src/contracts/Repository/Values/Content/SectionStruct.php
+++ src/contracts/Repository/Values/Content/SectionStruct.php
@@ -16,15 +16,11 @@ abstract class SectionStruct extends ValueObject
      * If set the Unique identifier of the section is changes.
      *
      * Needs to be a unique Section->identifier string value.
-     *
-     * @var string
      */
-    public $identifier;
+    public ?string $identifier;
 
     /**
      * If set the name of the section is changed.
-     *
-     * @var string
      */
-    public $name;
+    public ?string $name;
 }
