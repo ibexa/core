d4fe1368ce6f8c723aa77bf60910eee109a786d8
Adam Wójs <adam@wojs.pl>
2025-06-01 08:18:46 +0200

    Added missing type hints to Ibexa\Contracts\Core\Repository\Values\Content\Relation

--- src/contracts/Repository/Values/Content/Relation.php
+++ src/contracts/Repository/Values/Content/Relation.php
@@ -24,63 +24,60 @@ abstract class Relation extends ValueObject
     /**
      * The relation type COMMON is a general relation between object set by a user.
      *
-     * @var int
-     *
      * @deprecated 5.0.0 const is deprecated and will be removed in 6.0.0. Use {@see RelationType::COMMON} instead.
      */
-    public const COMMON = 1;
+    public const int COMMON = 1;
 
     /**
      * the relation type EMBED is set for a relation which is anchored as embedded link in an attribute value.
      *
-     * @var int
-     *
      * @deprecated 5.0.0 const is deprecated and will be removed in 6.0.0. Use {@see RelationType::EMBED} instead.
      */
-    public const EMBED = 2;
+    public const int EMBED = 2;
 
     /**
      * the relation type LINK is set for a relation which is anchored as link in an attribute value.
      *
-     * @var int
-     *
      * @deprecated 5.0.0 const is deprecated and will be removed in 6.0.0. Use {@see RelationType::LINK} instead.
      */
-    public const LINK = 4;
+    public const int LINK = 4;
 
     /**
      * the relation type FIELD is set for a relation which is part of an relation attribute value.
      *
-     * @var int
-     *
      * @deprecated 5.0.0 const is deprecated and will be removed in 6.0.0. Use {@see RelationType::FIELD} instead.
      */
-    public const FIELD = 8;
+    public const int FIELD = 8;
 
     /**
      * the relation type ASSET is set for a relation to asset in an attribute value.
      *
-     * @var int
-     *
      * @deprecated 5.0.0 const is deprecated and will be removed in 6.0.0. Use {@see RelationType::ASSET} instead.
      */
-    public const ASSET = 16;
+    public const int ASSET = 16;
 
     /**
-     * Id of the relation.
-     *
-     * @var mixed
+     * ID of the relation.
      */
-    protected $id;
+    protected int $id;
 
     /**
-     * Source content type Field Definition Id.
-     * For relation not of type RelationType::COMMON this field denotes the field definition id
+     * Source content type Field Definition identifier.
+     * For relation not of type RelationType::COMMON this field denotes the field definition identifier
      * of the attribute where the relation is anchored.
-     *
-     * @var string
      */
-    protected $sourceFieldDefinitionIdentifier;
+    protected ?string $sourceFieldDefinitionIdentifier = null;
+
+    /**
+     * The relation type bitmask.
+     *
+     * @see Relation::COMMON
+     * @see Relation::EMBED
+     * @see Relation::LINK
+     * @see Relation::FIELD
+     * @see Relation::ASSET
+     */
+    protected int $type;
 
     /**
      * the content of the source content of the relation.
@@ -95,17 +92,4 @@ abstract class Relation extends ValueObject
      * @return \Ibexa\Contracts\Core\Repository\Values\Content\ContentInfo
      */
     abstract public function getDestinationContentInfo(): ContentInfo;
-
-    /**
-     * The relation type bitmask.
-     *
-     * @see Relation::COMMON
-     * @see Relation::EMBED
-     * @see Relation::LINK
-     * @see Relation::FIELD
-     * @see Relation::ASSET
-     *
-     * @var int
-     */
-    protected $type;
 }
