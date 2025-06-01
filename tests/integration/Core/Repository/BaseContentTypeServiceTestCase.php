449960aec0f6a7de9b27cfa11cfab3d8ab82a6e3
Andrew Longosz <alongosz@users.noreply.github.com>
2025-06-02 01:05:42 +0200

    [Tests] Added `Case` suffix to the base content type service integration test case

--- tests/integration/Core/Repository/BaseContentTypeServiceTestCase.php
+++ tests/integration/Core/Repository/BaseContentTypeServiceTestCase.php
@@ -12,7 +12,7 @@ use Ibexa\Contracts\Core\Repository\Values\Content\Location;
 /**
  * Base class for content type specific tests.
  */
-abstract class BaseContentTypeServiceTest extends BaseTestCase
+abstract class BaseContentTypeServiceTestCase extends BaseTestCase
 {
     /**
      * Creates a fully functional ContentTypeDraft and returns it.
