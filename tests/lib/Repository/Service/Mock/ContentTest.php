7649c35716c5c4c97ba60ec926e880146237f91c
Adam Wójs <adam@wojs.pl>
2025-06-02 14:17:02 +0200

    [Tests] Dropped obsolete  Ibexa\Tests\Core\Repository\Service\Mock\ContentTest::testCreateContentThrowsInvalidArgumentExceptionMainLanguageCodeNotSet test

--- tests/lib/Repository/Service/Mock/ContentTest.php
+++ tests/lib/Repository/Service/Mock/ContentTest.php
@@ -1043,20 +1043,6 @@ class ContentTest extends BaseServiceMockTest
      *
      * @covers \Ibexa\Contracts\Core\Repository\ContentService::createContent
      */
-    public function testCreateContentThrowsInvalidArgumentExceptionMainLanguageCodeNotSet()
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Argument \'$contentCreateStruct\' is invalid: the \'mainLanguageCode\' property must be set');
-
-        $mockedService = $this->getPartlyMockedContentService();
-        $mockedService->createContent(new ContentCreateStruct(), []);
-    }
-
-    /**
-     * Test for the createContent() method.
-     *
-     * @covers \Ibexa\Contracts\Core\Repository\ContentService::createContent
-     */
     public function testCreateContentThrowsUnauthorizedException()
     {
         $this->expectException(UnauthorizedException::class);
