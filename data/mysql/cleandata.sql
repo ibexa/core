690baad458f91fb1a4246d73d1c04d5725ad35f4
Dawid Parafiński <dawid.parafinski@ibexa.co>
2025-06-09 08:09:52 +0200

    IBX-9941: Fixed references to ibexa_preferences table clean data (#586)

--- data/mysql/cleandata.sql
+++ data/mysql/cleandata.sql
@@ -300,5 +300,5 @@ INSERT INTO `ibexa_user_setting` (`is_enabled`, `max_login`, `user_id`)
 VALUES (1,1000,10),
        (1,10,14);
 
-INSERT INTO `ibexa_preferences` (`name`, `user_id`, `value`)
+INSERT INTO `ibexa_user_preference` (`name`, `user_id`, `value`)
 SELECT 'focus_mode', u.contentobject_id, '0' FROM `ibexa_user` u WHERE u.login = 'admin';
