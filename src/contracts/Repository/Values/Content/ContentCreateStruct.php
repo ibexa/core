b2f31927aae7cee684c813c28028bc33bd11ae7b
Andrew Longosz <alongosz@users.noreply.github.com>
2025-06-25 11:35:11 +0200

    [PHPDoc] Fixed grammar of ContentCreateStruct

--- src/contracts/Repository/Values/Content/ContentCreateStruct.php
+++ src/contracts/Repository/Values/Content/ContentCreateStruct.php
@@ -28,25 +28,25 @@ abstract class ContentCreateStruct extends ContentStruct
     /**
      * The main language code for the content. This language will also
      * be used as an initial language for the first created version.
-     * It is also used as default language for added fields.
+     * It is also used as the default language for added fields.
      *
      * Required.
      */
     public string $mainLanguageCode;
 
     /**
-     * The section the content is assigned to.
-     * If not set the section of the parent is used or a default section.
+     * The section to which the content is assigned.
+     * If not set, either the parent section or a default section is used.
      */
     public ?int $sectionId = null;
 
     /**
-     * The owner of the content. If not given the current authenticated user is set as owner.
+     * The owner of the content. If not given, the current authenticated user is set as owner.
      */
     public ?int $ownerId = null;
 
     /**
-     * Indicates if the content object is shown in the mainlanguage if its not present in an other requested language.
+     * Indicates if the content object is shown in the main language if it's not present in another requested language.
      */
     public ?bool $alwaysAvailable = null;
 
@@ -58,7 +58,7 @@ abstract class ContentCreateStruct extends ContentStruct
     public ?string $remoteId = null;
 
     /**
-     * Modification date. If not given the current timestamp is used.
+     * Modification date. If not given, the current timestamp is used.
      */
     public ?DateTimeInterface $modificationDate = null;
 }
