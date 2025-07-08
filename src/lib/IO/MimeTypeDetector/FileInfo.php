ef1fdb93f10f32aebd2349c866f3b710d5d2480c
Andrew Longosz <alongosz@users.noreply.github.com>
2025-07-08 16:02:35 +0200

    Added missing type hints to IO\MimeTypeDetector\FileInfo

--- src/lib/IO/MimeTypeDetector/FileInfo.php
+++ src/lib/IO/MimeTypeDetector/FileInfo.php
@@ -4,19 +4,20 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Core\IO\MimeTypeDetector;
 
+use finfo;
+use Ibexa\Contracts\Core\Exception\InvalidArgumentException;
 use Ibexa\Contracts\Core\IO\MimeTypeDetector;
 
 class FileInfo implements MimeTypeDetector
 {
     /**
      * Magic FileInfo object.
-     *
-     * @var \finfo
      */
-    protected $fileInfo;
+    protected finfo $fileInfo;
 
     /**
      * Checks for the required ext/fileinfo.
@@ -30,25 +31,33 @@ class FileInfo implements MimeTypeDetector
         }
     }
 
-    public function getFromPath($path): string|false
+    public function getFromPath(string $path): string
     {
-        return $this->getFileInfo()->file($path);
+        $fileInfo = $this->getFileInfo()->file($path);
+        if ($fileInfo === false) {
+            throw new InvalidArgumentException('$path', "Failed to get file information for the path '$path'");
+        }
+
+        return $fileInfo;
     }
 
-    public function getFromBuffer($path): string|false
+    public function getFromBuffer(string $buffer): string
     {
-        return $this->getFileInfo()->buffer($path);
+        $bufferInfo = $this->getFileInfo()->buffer($buffer);
+        if ($bufferInfo === false) {
+            throw new InvalidArgumentException('$path', 'Failed to get file information a string buffer');
+        }
+
+        return $bufferInfo;
     }
 
     /**
      * Creates a new (or re-uses) finfo object and returns it.
-     *
-     * @return \finfo
      */
-    protected function getFileInfo()
+    protected function getFileInfo(): finfo
     {
         if (!isset($this->fileInfo)) {
-            $this->fileInfo = new \finfo(FILEINFO_MIME_TYPE);
+            $this->fileInfo = new finfo(FILEINFO_MIME_TYPE);
         }
 
         return $this->fileInfo;
