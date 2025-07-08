c080dca44c0822792f94eb4732b0cf3c64831510
Andrew Longosz <alongosz@users.noreply.github.com>
2025-07-08 16:01:42 +0200

    Aligned NotFoundException `$previous` arg type with `\Exception` signature

--- src/lib/IO/Exception/IOException.php
+++ src/lib/IO/Exception/IOException.php
@@ -4,18 +4,19 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Core\IO\Exception;
 
-use Exception;
 use RuntimeException;
+use Throwable;
 
 /**
  * General IO exception.
  */
 class IOException extends RuntimeException
 {
-    public function __construct($message, Exception $e = null)
+    public function __construct(string $message, ?Throwable $e = null)
     {
         parent::__construct($message, 0, $e);
     }
