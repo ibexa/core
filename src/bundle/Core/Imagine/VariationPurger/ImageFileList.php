d252faac2fd295116f8f59a1c241e104f7a82e3d
Andrew Longosz <alongosz@users.noreply.github.com>
2025-07-09 11:18:59 +0200

    Added missing type hints and improved code of Imagine VariationPurger

--- src/bundle/Core/Imagine/VariationPurger/ImageFileList.php
+++ src/bundle/Core/Imagine/VariationPurger/ImageFileList.php
@@ -4,6 +4,7 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Bundle\Core\Imagine\VariationPurger;
 
@@ -12,6 +13,8 @@ use Iterator;
 
 /**
  * Iterates over BinaryFile id entries for original images.
+ *
+ * @extends \Iterator<array-key, string>
  */
 interface ImageFileList extends Countable, Iterator
 {
