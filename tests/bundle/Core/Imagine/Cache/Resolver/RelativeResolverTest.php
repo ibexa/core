a97b96e4c855c2bffdb95923dbdb7f09899034fd
Andrew Longosz <alongosz@users.noreply.github.com>
2025-07-16 15:57:06 +0200

    [Tests] Added missing type hints to `Imagine\Cache` Resolver tests

--- tests/bundle/Core/Imagine/Cache/Resolver/RelativeResolverTest.php
+++ tests/bundle/Core/Imagine/Cache/Resolver/RelativeResolverTest.php
@@ -4,17 +4,21 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Tests\Bundle\Core\Imagine\Cache\Resolver;
 
 use Ibexa\Bundle\Core\Imagine\Cache\Resolver\RelativeResolver;
 use Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
-class RelativeResolverTest extends TestCase
+/**
+ * @covers \Ibexa\Bundle\Core\Imagine\Cache\Resolver\RelativeResolver
+ */
+final class RelativeResolverTest extends TestCase
 {
-    /** @var \PHPUnit\Framework\MockObject\MockObject|\Liip\ImagineBundle\Imagine\Cache\Resolver\ResolverInterface */
-    private $liipResolver;
+    private ResolverInterface & MockObject $liipResolver;
 
     protected function setUp(): void
     {
@@ -22,14 +26,14 @@ class RelativeResolverTest extends TestCase
         $this->liipResolver = $this->getMockBuilder(ResolverInterface::class)->getMock();
     }
 
-    public function testResolve()
+    public function testResolve(): void
     {
         $resolver = new RelativeResolver($this->liipResolver);
 
         $path = '7/4/2/0/247-1-eng-GB/test.png';
         $filter = 'big';
 
-        $absolute = 'http://ibexa.co/var/site/storage/images/_aliases/big/7/4/2/0/247-1-eng-GB/test.png';
+        $absolute = 'https://ibexa.co/var/site/storage/images/_aliases/big/7/4/2/0/247-1-eng-GB/test.png';
         $expected = '/var/site/storage/images/_aliases/big/7/4/2/0/247-1-eng-GB/test.png';
 
         $this->liipResolver
