85cad420cd05dd203a40df881156632e0b63bfde
Andrew Longosz <alongosz@users.noreply.github.com>
2025-07-16 15:45:15 +0200

    Added missing type hints to `Imagine\Filter\FilterInterface` and its implementations

--- src/bundle/Core/Imagine/Filter/Imagick/SwirlFilter.php
+++ src/bundle/Core/Imagine/Filter/Imagick/SwirlFilter.php
@@ -4,24 +4,25 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Bundle\Core\Imagine\Filter\Imagick;
 
 use Ibexa\Bundle\Core\Imagine\Filter\AbstractFilter;
 use Imagine\Image\ImageInterface;
+use Imagine\Imagick\Image;
 
 class SwirlFilter extends AbstractFilter
 {
     /**
-     * @param \Imagine\Image\ImageInterface|\Imagine\Imagick\Image $image
-     *
-     * @return \Imagine\Image\ImageInterface
+     * @throws \ImagickException
      */
-    public function apply(ImageInterface $image)
+    public function apply(ImageInterface $image): Image|ImageInterface
     {
-        /** @var \Imagick $imagick */
-        $imagick = $image->getImagick();
-        $imagick->swirlImage((float)$this->getOption('degrees', 60));
+        if ($image instanceof Image) {
+            $imagick = $image->getImagick();
+            $imagick->swirlImage((float)$this->getOption('degrees', 60));
+        }
 
         return $image;
     }
