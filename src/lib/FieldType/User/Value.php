1daec34bc60054797b16af08366709972e74678a
Andrew Longosz <alongosz@users.noreply.github.com>
2025-07-24 15:55:36 +0200

    Aligned User field type implementation with contracts changes

--- src/lib/FieldType/User/Value.php
+++ src/lib/FieldType/User/Value.php
@@ -4,84 +4,52 @@
  * @copyright Copyright (C) Ibexa AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
 
 namespace Ibexa\Core\FieldType\User;
 
+use DateTimeImmutable;
 use Ibexa\Core\FieldType\Value as BaseValue;
 
 /**
- * Value for User field type.
+ * Value for the User field type.
  */
 class Value extends BaseValue
 {
     /**
      * Has stored login.
-     *
-     * @var bool
      */
-    public $hasStoredLogin;
+    public bool $hasStoredLogin;
 
-    /**
-     * Contentobject id.
-     *
-     * @var mixed
-     */
-    public $contentId;
+    public int $contentId;
 
-    /**
-     * Login.
-     *
-     * @var string
-     */
-    public $login;
+    public string $login;
 
     /**
      * Email.
-     *
-     * @var string
      */
-    public $email;
+    public string $email;
 
-    /**
-     * Password hash.
-     *
-     * @var string
-     */
-    public $passwordHash;
+    public string $passwordHash;
 
-    /**
-     * Password hash type.
-     *
-     * @var mixed
-     */
-    public $passwordHashType;
+    public int $passwordHashType;
 
-    /**
-     * @var \DateTimeImmutable|null
-     */
-    public $passwordUpdatedAt;
+    public ?DateTimeImmutable $passwordUpdatedAt;
 
-    /**
-     * Is enabled.
-     *
-     * @var bool
-     */
-    public $enabled;
+    public bool $enabled;
 
     /**
      * Max login.
-     *
-     * @var int
      */
-    public $maxLogin;
+    public int $maxLogin;
 
     /**
-     * @var string Write only property, takes a plain password for use when creating user or updating password.
+     * @var string Write-only property, takes a plain password for use when creating user or updating password.
      */
-    public $plainPassword;
+    public string $plainPassword;
 
-    public function __toString()
+    public function __toString(): string
     {
-        return (string)$this->login;
+        return $this->login;
     }
 }
