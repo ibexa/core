f91165b59640547dfb7c7b2e781d5d409111c853
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2025-08-13 11:06:32 +0200

    IBX-10458: Fixup

--- tests/lib/Repository/SiteAccessAware/ContentTypeServiceTest.php
+++ tests/lib/Repository/SiteAccessAware/ContentTypeServiceTest.php
@@ -13,6 +13,7 @@ use Ibexa\Contracts\Core\Repository\Values\ContentType\ContentTypeUpdateStruct;
 use Ibexa\Contracts\Core\Repository\Values\ContentType\FieldDefinitionCreateStruct;
 use Ibexa\Contracts\Core\Repository\Values\ContentType\FieldDefinitionUpdateStruct;
 use Ibexa\Contracts\Core\Repository\Values\ContentType\Query\ContentTypeQuery;
+use Ibexa\Contracts\Core\Repository\Values\ContentType\SearchResult;
 use Ibexa\Core\Repository\SiteAccessAware\ContentTypeService;
 use Ibexa\Core\Repository\Values\ContentType\ContentType;
 use Ibexa\Core\Repository\Values\ContentType\ContentTypeCreateStruct;
@@ -101,7 +102,7 @@ class ContentTypeServiceTest extends AbstractServiceTest
 
             ['deleteUserDrafts', [14], null],
 
-            ['findContentTypes', [new ContentTypeQuery()], [$contentType]],
+            ['findContentTypes', [new ContentTypeQuery()], new SearchResult()],
 
             ['countContentTypes', [new ContentTypeQuery()], 1],
         ];
