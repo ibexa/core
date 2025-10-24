2590376d08fb3b6dd2dbcb11ab2c29687ab61f74
Andrew Longosz <alongosz@users.noreply.github.com>
2025-10-24 14:30:30 +0200

    [Contracts] Fixed PHPDoc type hints of `Repository\Validator\ContentValidator`

--- src/contracts/Repository/Validator/ContentValidator.php
+++ src/contracts/Repository/Validator/ContentValidator.php
@@ -8,9 +8,13 @@ declare(strict_types=1);
 
 namespace Ibexa\Contracts\Core\Repository\Validator;
 
+use Ibexa\Contracts\Core\FieldType\ValidationError;
 use Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException;
 use Ibexa\Contracts\Core\Repository\Values\ValueObject;
 
+/**
+ * @phpstan-type TValidationErrorList array<int, array<string, ValidationError|ValidationError[]>>
+ */
 interface ContentValidator
 {
     public function supports(ValueObject $object): bool;
@@ -18,19 +22,13 @@ interface ContentValidator
     /**
      * Validates given content related ValueObject returning field errors structure as a result.
      *
-     * @param array $context Additional context parameters to be used by validators.
+     * @param array<string, mixed> $context Additional context parameters to be used by validators.
      * @param string[]|null $fieldIdentifiers List of field identifiers for partial validation or null for
      *                      case of full validation. Empty identifiers array is equal to no validation.
      *
-     * @phpstan-return array<
-     *     int,
-     *     array<
-     *         string,
-     *         \Ibexa\Contracts\Core\FieldType\ValidationError|\Ibexa\Contracts\Core\FieldType\ValidationError[]
-     *     >
-     * >
+     * @phpstan-return TValidationErrorList
      *
-     * @return array Grouped validation errors by field definition ID and language code, in format:
+     * @return array<int, array<string, ValidationError|ValidationError[]>> Grouped validation errors by field definition ID and language code, in format:
      *           $returnValue[int $fieldDefinitionId][string $languageCode] = $fieldErrors;
      *
      * @throws InvalidArgumentException
