812012225bafa761500057325181f818f1e332ab
Thorsten Reiter <thorsten.reiter@ibexa.co>
2025-10-28 16:34:58 +0100

    adapt error message

--- src/lib/Base/Exceptions/ContentFieldValidationException.php
+++ src/lib/Base/Exceptions/ContentFieldValidationException.php
@@ -59,10 +59,10 @@ class ContentFieldValidationException extends APIContentFieldValidationException
         $exception = new self($errors);
         $exception->contentName = $contentName;
 
-        $exception->setMessageTemplate('Content "%contentName%" fields did not validate: %errors%');
+        $exception->setMessageTemplate('Content%contentName%fields did not validate: %errors%');
         $exception->setParameters([
             '%errors%' => $exception->generateValidationErrorsMessages(),
-            '%contentName%' => $exception->contentName !== null ? $exception->contentName : '',
+            '%contentName%' => $exception->contentName !== null ? ' "'. $exception->contentName. '" ' : ' ',
         ]);
         $exception->message = $exception->getBaseTranslation();
 
