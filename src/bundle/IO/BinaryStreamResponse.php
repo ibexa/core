301afa5213083cb4d1e72974dede767a6ebe6451
Sztig <sebastianjamula@gmail.com>
2026-01-02 16:40:15 +0100

    made the files to return 206 even if providing full file, redundancy adjustments

--- src/bundle/IO/BinaryStreamResponse.php
+++ src/bundle/IO/BinaryStreamResponse.php
@@ -179,14 +179,16 @@ class BinaryStreamResponse extends Response
                 if ($start <= $end) {
                     if ($start < 0 || $end > $fileSize - 1) {
                         $this->setStatusCode(416); // HTTP_REQUESTED_RANGE_NOT_SATISFIABLE
-                    } elseif ($start !== 0 || $end !== $fileSize - 1) {
-                        $this->maxlen = $end < $fileSize ? $end - $start + 1 : -1;
-                        $this->offset = $start;
 
-                        $this->setStatusCode(206); // HTTP_PARTIAL_CONTENT
-                        $this->headers->set('Content-Range', sprintf('bytes %s-%s/%s', $start, $end, $fileSize));
-                        $this->headers->set('Content-Length', $end - $start + 1);
+                        return $this;
                     }
+
+                    $this->maxlen = $end - $start + 1;
+                    $this->offset = $start;
+
+                    $this->setStatusCode(206); // HTTP_PARTIAL_CONTENT
+                    $this->headers->set('Content-Range', sprintf('bytes %s-%s/%s', $start, $end, $fileSize));
+                    $this->headers->set('Content-Length', $this->maxlen);
                 }
             }
         }
