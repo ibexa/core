e456486b67f3060da77d2725c2443de953ba4aa7
MateuszKolankowski <wiewiurdp@tlen. pl>
2026-01-07 10:06:28 +0100

    Remove dependency from getLocationChildCount test

--- tests/integration/Core/Repository/LocationServiceTest.php
+++ tests/integration/Core/Repository/LocationServiceTest.php
@@ -1145,8 +1145,6 @@ class LocationServiceTest extends BaseTestCase
      * Test for the getLocationChildCount() method with a limitation on the number of children.
      *
      * @covers \Ibexa\Contracts\Core\Repository\LocationService::getLocationChildCount()
-     *
-     * @depends testLoadLocation
      */
     public function testGetLocationChildCountWithLimitation(): void
     {
