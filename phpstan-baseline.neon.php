a73d00262f4d566d4c152966123cccd713594eaa
MateuszKolankowski <wiewiurdp@tlen. pl>
2026-01-27 00:56:14 +0100

    Added PHP 8.4 to CI configuration and update phpstan baseline files

---
+++ phpstan-baseline.neon.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+$includes = [];
+if (PHP_VERSION_ID < 80400) {
+    $includes[] = __DIR__ . '/phpstan-baseline-8.3.neon';
+} else {
+    $includes[] = __DIR__ . '/phpstan-baseline-8.4.neon';
+}
+
+$config = [];
+$config['includes'] = $includes;
+
+return $config;
