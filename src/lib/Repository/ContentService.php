300106f36472650cacee9c89548adfe0d8e15428
Vidar Langseid <vl@ibexa.co>
2026-02-10 12:35:51 +0100

    fixup! IBX-10854: Not possible to hide content draft

--- src/lib/Repository/ContentService.php
+++ src/lib/Repository/ContentService.php
@@ -2528,17 +2528,17 @@ class ContentService implements ContentServiceInterface
      */
     public function hideContent(ContentInfo $contentInfo): void
     {
-        // If ContentInfo is in draft state, mainLocationId is yet not set
-        if (!$contentInfo->isDraft()) {
-            $locationTarget = (new DestinationLocationTarget($contentInfo->mainLocationId, $contentInfo));
-            if (!$this->permissionResolver->canUser(
-                'content',
-                'hide',
-                $contentInfo,
-                [$locationTarget]
-            )) {
-                throw new UnauthorizedException('content', 'hide', ['contentId' => $contentInfo->id]);
-            }
+        // If ContentInfo is in draft state, mainocationId is yet not set
+        $locationTarget = !$contentInfo->isDraft()
+            ? [new DestinationLocationTarget($contentInfo->mainLocationId, $contentInfo)]
+            : [];
+        if (!$this->permissionResolver->canUser(
+            'content',
+            'hide',
+            $contentInfo,
+            $locationTarget
+        )) {
+            throw new UnauthorizedException('content', 'hide', ['contentId' => $contentInfo->id]);
         }
 
         $this->repository->beginTransaction();
@@ -2572,16 +2572,17 @@ class ContentService implements ContentServiceInterface
     public function revealContent(ContentInfo $contentInfo): void
     {
         // If ContentInfo is in draft state, mainLocationId is yet not set
-        if (!$contentInfo->isDraft()) {
-            $locationTarget = (new DestinationLocationTarget($contentInfo->mainLocationId, $contentInfo));
-            if (!$this->permissionResolver->canUser(
-                'content',
-                'hide',
-                $contentInfo,
-                [$locationTarget]
-            )) {
-                throw new UnauthorizedException('content', 'hide', ['contentId' => $contentInfo->id]);
-            }
+        $locationTarget = !$contentInfo->isDraft()
+            ? [new DestinationLocationTarget($contentInfo->mainLocationId, $contentInfo)]
+            : [];
+
+        if (!$this->permissionResolver->canUser(
+            'content',
+            'hide',
+            $contentInfo,
+            [$locationTarget]
+        )) {
+            throw new UnauthorizedException('content', 'hide', ['contentId' => $contentInfo->id]);
         }
 
         $this->repository->beginTransaction();
