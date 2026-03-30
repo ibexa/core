c4fc78ec8f4c28984de253bc8732c8067acac99d
konradoboza <konrad.oboza@ibexa.co>
2026-03-30 14:00:19 +0200

    IBX-11474: Added `ibexa/cloud` existence check for the existing projects

---
+++ tests/bundle/Core/IbexaCoreBundleTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Ibexa\Tests\Bundle\Core;
+
+use Ibexa\Bundle\Core\IbexaCoreBundle;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+/**
+ * @covers \Ibexa\Bundle\Core\IbexaCoreBundle
+ */
+final class IbexaCoreBundleTest extends TestCase
+{
+    private IbexaCoreBundle $bundle;
+
+    protected function setUp(): void
+    {
+        $this->bundle = new IbexaCoreBundle();
+    }
+
+    protected function tearDown(): void
+    {
+        unset($_SERVER['PLATFORM_RELATIONSHIPS']);
+    }
+
+    public function testBuildDoesNotThrowWhenNotOnCloud(): void
+    {
+        unset($_SERVER['PLATFORM_RELATIONSHIPS']);
+
+        $container = new ContainerBuilder();
+        // No exception expected
+        $this->bundle->build($container);
+
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testBuildDoesNotThrowWhenOnCloudWithIbexaCloudExtension(): void
+    {
+        $_SERVER['PLATFORM_RELATIONSHIPS'] = 'some_value';
+
+        $container = new ContainerBuilder();
+        $container->registerExtension($this->createIbexaCloudExtension());
+
+        $this->bundle->build($container);
+
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testBuildThrowsWhenOnCloudWithoutIbexaCloudExtension(): void
+    {
+        $_SERVER['PLATFORM_RELATIONSHIPS'] = 'some_value';
+
+        $container = new ContainerBuilder();
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('The package `ibexa/cloud` is mandatory for Ibexa Cloud deployments.');
+
+        $this->bundle->build($container);
+    }
+
+    private function createIbexaCloudExtension(): Extension
+    {
+        return new class() extends Extension {
+            public function getAlias(): string
+            {
+                return 'ibexa_cloud';
+            }
+
+            public function load(array $configs, ContainerBuilder $container): void
+            {
+            }
+        };
+    }
+}
