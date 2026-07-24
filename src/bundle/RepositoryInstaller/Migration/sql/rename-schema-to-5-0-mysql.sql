a7b754222f7a6bc78a62019ba2115f57ffa542a9
Paweł Niedzielski <pawel.tadeusz.niedzielski@gmail.com>
2026-07-24 13:44:48 +0200

    IBX-11939: Added data-value rename statements dropped during rebase conflict resolution
    Restores the ez_lock->ibexa_lock and ezstring->ibexa_string (x2) UPDATE
    statements that were present in the original commit but got lost while
    resolving rebase conflicts against 6.0.

--- src/bundle/RepositoryInstaller/Migration/sql/rename-schema-to-5-0-mysql.sql
+++ src/bundle/RepositoryInstaller/Migration/sql/rename-schema-to-5-0-mysql.sql
@@ -345,3 +345,9 @@ ALTER TABLE ibexa_content_relation RENAME COLUMN contentclassattribute_id TO con
 ALTER TABLE ibexa_content_relation RENAME INDEX ibexa_content_relation_cca_id TO ibexa_content_relation_ccfd_id;
 -- ibexa:sql-statement-separator
 ALTER TABLE ibexa_search_object_word_link RENAME COLUMN contentclass_attribute_id TO content_type_field_definition_id;
+-- ibexa:sql-statement-separator
+UPDATE ibexa_object_state_group SET identifier = 'ibexa_lock' WHERE identifier = 'ez_lock';
+-- ibexa:sql-statement-separator
+UPDATE ibexa_content_type_field_definition SET data_type_string = 'ibexa_string' WHERE data_type_string = 'ezstring';
+-- ibexa:sql-statement-separator
+UPDATE ibexa_content_field SET data_type_string = 'ibexa_string' WHERE data_type_string = 'ezstring';
--- src/bundle/RepositoryInstaller/Migration/sql/rename-schema-to-5-0-postgresql.sql
+++ src/bundle/RepositoryInstaller/Migration/sql/rename-schema-to-5-0-postgresql.sql
@@ -407,3 +407,9 @@ ALTER SEQUENCE ezurlwildcard_id_seq RENAME TO ibexa_url_wildcard_id_seq;
 ALTER SEQUENCE ezuser_accountkey_id_seq RENAME TO ibexa_user_accountkey_id_seq;
 -- ibexa:sql-statement-separator
 ALTER SEQUENCE ezuser_role_id_seq RENAME TO ibexa_user_role_id_seq;
+-- ibexa:sql-statement-separator
+UPDATE ibexa_object_state_group SET identifier = 'ibexa_lock' WHERE identifier = 'ez_lock';
+-- ibexa:sql-statement-separator
+UPDATE ibexa_content_type_field_definition SET data_type_string = 'ibexa_string' WHERE data_type_string = 'ezstring';
+-- ibexa:sql-statement-separator
+UPDATE ibexa_content_field SET data_type_string = 'ibexa_string' WHERE data_type_string = 'ezstring';
