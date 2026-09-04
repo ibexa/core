0b493aad47d6bef5ccc878cebee1093c27252bbc
Vidar Langseid <vidar.langseid@ibexa.co>
2026-09-04 13:59:02 +0200

    Normalized stored Limitation values in MemberOf and Role acceptValue()

--- src/lib/Limitation/RoleLimitationType.php
+++ src/lib/Limitation/RoleLimitationType.php
@@ -48,7 +48,10 @@ final class RoleLimitationType extends AbstractPersistenceLimitationType impleme
         }
 
         foreach ($limitationValue->limitationValues as $key => $id) {
-            if (!is_int($id)) {
+            // Cast integers passed as string to int, as they are when read back from storage
+            if (is_string($id) && preg_match('/^\d+$/', $id) === 1) {
+                $limitationValue->limitationValues[$key] = (int)$id;
+            } elseif (!is_int($id)) {
                 throw new InvalidArgumentType("\$limitationValue->limitationValues[{$key}]", 'int', $id);
             }
         }
