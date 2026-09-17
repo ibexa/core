ff62442be1d9566bf1cffed36cfbe8166bb6ca46
Dawid Parafinski <dawid.parafinski@ibexa.co>
2026-09-17 15:31:48 +0200

    IBX-12606: Used mocks instead of stubs for URL handlers with expectations in URLCheckerTest

--- tests/bundle/Core/URLChecker/URLCheckerTest.php
+++ tests/bundle/Core/URLChecker/URLCheckerTest.php
@@ -55,8 +55,8 @@ class URLCheckerTest extends TestCase
             ->willReturn($this->createSearchResults($groups));
 
         $handlers = [
-            'http' => self::createStub(URLHandlerInterface::class),
-            'https' => self::createStub(URLHandlerInterface::class),
+            'http' => $this->createMock(URLHandlerInterface::class),
+            'https' => $this->createMock(URLHandlerInterface::class),
         ];
 
         foreach ($handlers as $scheme => $handler) {
@@ -91,7 +91,7 @@ class URLCheckerTest extends TestCase
             ->with('Unsupported URL schema: https');
 
         $handlers = [
-            'http' => self::createStub(URLHandlerInterface::class),
+            'http' => $this->createMock(URLHandlerInterface::class),
         ];
 
         foreach ($handlers as $scheme => $handler) {
